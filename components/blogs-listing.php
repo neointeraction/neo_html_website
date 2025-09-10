e73e9976a378a56444d71761798f37382967204c
mshameer237 <mshameer237@gmail.com>
2025-09-11 00:52:59 +0530

    updated blog listing page
    updated blog listing page

--- components/blogs-listing.php
+++ components/blogs-listing.php
@@ -1,48 +1,56 @@
-         <!-- Card 1 -->
-          <div class="col-12 col-sm-6 col-md-4 col-lg-3" >
-            <div class="blog-card h-100">
-              <img src="assets/images/temp/blog.png" class="card-img-top" alt="Dashboard UI">
-              <div class="card-body">
-                <h5 class="blog-card-title">How to design Effective Dashboard to help boost your...</h5>
-                <p class="blog-card-text">
-                  <small class="text-muted">Thirunavukkarasu L • 1 day ago</small>
-                </p>
-              </div>
-            </div>
-          </div>
-          <!-- Card 2 -->
-          <div class="col-12 col-sm-6 col-md-4 col-lg-3" >
-            <div class="blog-card h-100">
-              <img src="assets/images/temp/blog2.png" class="card-img-top" alt="Dashboard UI">
-              <div class="card-body">
-                <h5 class="blog-card-title">Advantages and disadvantages of Website Templates</h5>
-                <p class="blog-card-text">
-                  <small class="text-muted">Rahul Kale • 6 days ago</small>
-                </p>
-              </div>
-            </div>
-          </div>
-          <!-- Card 3 -->
-          <div class="col-12 col-sm-6 col-md-4 col-lg-3" >
-            <div class="blog-card h-100">
-              <img src="assets/images/temp/blog.png" class="card-img-top" alt="Dashboard UI">
-              <div class="card-body">
-                <h5 class="blog-card-title">What Is UX Design Process? A Complete Actionable Guide</h5>
-                <p class="blog-card-text">
-                  <small class="text-muted">Rahul Kale • 6 days ago</small>
-                </p>
-              </div>
-            </div>
-          </div>
-          <!-- Card 4 -->
-          <div class="col-12 col-sm-6 col-md-4 col-lg-3" >
-            <div class="blog-card h-100">
-              <img src="assets/images/temp/blog2.png" class="card-img-top" alt="Dashboard UI">
-              <div class="card-body">
-                <h5 class="blog-card-title">What Is UX Design Process? A Complete Actionable Guide</h5>
-                <p class="blog-card-text">
-                  <small class="text-muted">Rahul Kale • 6 days ago</small>
-                </p>
-              </div>
-            </div>
-          </div>
+       <!-- Card 1 -->
+       <div class="col-12 col-sm-6 col-md-4 col-lg-3">
+         <a href="#" class="blog-anchor">
+           <div class="blog-card h-100">
+             <img src="assets/images/temp/blog.png" class="card-img-top" alt="Dashboard UI">
+             <div class="card-body">
+               <h5 class="blog-card-title">How to design Effective Dashboard to help boost your...</h5>
+               <p class="blog-card-text">
+                 <small class="text-muted">Thirunavukkarasu L • 1 day ago</small>
+               </p>
+             </div>
+           </div>
+         </a>
+       </div>
+       <!-- Card 2 -->
+       <div class="col-12 col-sm-6 col-md-4 col-lg-3">
+         <a href="#" class="blog-anchor">
+           <div class="blog-card h-100">
+             <img src="assets/images/temp/blog2.png" class="card-img-top" alt="Dashboard UI">
+             <div class="card-body">
+               <h5 class="blog-card-title">Advantages and disadvantages of Website Templates</h5>
+               <p class="blog-card-text">
+                 <small class="text-muted">Rahul Kale • 6 days ago</small>
+               </p>
+             </div>
+           </div>
+         </a>
+       </div>
+       <!-- Card 3 -->
+       <div class="col-12 col-sm-6 col-md-4 col-lg-3">
+         <a href="#" class="blog-anchor">
+           <div class="blog-card h-100">
+             <img src="assets/images/temp/blog.png" class="card-img-top" alt="Dashboard UI">
+             <div class="card-body">
+               <h5 class="blog-card-title">What Is UX Design Process? A Complete Actionable Guide</h5>
+               <p class="blog-card-text">
+                 <small class="text-muted">Rahul Kale • 6 days ago</small>
+               </p>
+             </div>
+           </div>
+         </a>
+       </div>
+       <!-- Card 4 -->
+       <div class="col-12 col-sm-6 col-md-4 col-lg-3">
+         <a href="#" class="blog-anchor">
+           <div class="blog-card h-100">
+             <img src="assets/images/temp/blog2.png" class="card-img-top" alt="Dashboard UI">
+             <div class="card-body">
+               <h5 class="blog-card-title">What Is UX Design Process? A Complete Actionable Guide</h5>
+               <p class="blog-card-text">
+                 <small class="text-muted">Rahul Kale • 6 days ago</small>
+               </p>
+             </div>
+           </div>
+         </a>
+       </div>
