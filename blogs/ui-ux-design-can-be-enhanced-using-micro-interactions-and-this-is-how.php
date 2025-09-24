13023981126d35a80b32cd0005bb78d5b09b95da
jishnu krishnan <jishnukrishnan30@gmail.com>
2025-09-24 13:10:58 +0530

    added 2 more blogs

---
+++ blogs/ui-ux-design-can-be-enhanced-using-micro-interactions-and-this-is-how.php
@@ -0,0 +1,176 @@
+<!DOCTYPE html>
+<html class="no-js" lang="en">
+
+<head>
+  <?php $path = str_repeat('../', substr_count($_SERVER['REQUEST_URI'], '/') - 2); ?>
+  <?php include $path.'includes/favicon.php'; ?>
+  <?php include $path.'includes/css.php'; ?>
+  <?php include $path.'includes/head-additional-scripts.php'; ?>
+  <title>UI UX Design can be enhanced using micro interactions and this is how</title>
+  <meta name="description" content="Micro interactions are sole moments in product design that achieve a specific task, while boosting natural product flow."/>
+  <link rel="canonical" href="https://www.neointeraction.com/post/ui-ux-design-can-be-enhanced-using-micro-interactions-and-this-is-how"/>
+  <meta property="og:title" content="UI UX Design can be enhanced using micro interactions and this is how"/>
+  <meta property="og:description" content="In its most basic sense, microinteractions are individual moments in the product design that accomplish a particular task, while enhancing what is intended to be the natural product flow. They are what bridge UI and UX, where functionality meets design – and they are everywhere.We as consumers see and interact with these interactive UI components daily, even without knowing. A ‘Like’ button is one of the simplest examples. A few others are: • A simple scroll bar that appears in the user interfac"/>
+  <meta property="og:image" content="https://static.wixstatic.com/media/c380a9_c112b0e4668d4522b99d4bc8e3d1fec6~mv2.webp/v1/fill/w_941,h_727,al_c,q_85/c380a9_c112b0e4668d4522b99d4bc8e3d1fec6~mv2.webp"/>
+  <meta property="og:image:width" content="941"/>
+  <meta property="og:image:height" content="727"/>
+  <meta property="og:url" content="https://www.neointeraction.com/post/ui-ux-design-can-be-enhanced-using-micro-interactions-and-this-is-how"/>
+  <meta property="og:site_name" content="Neointeraction"/>
+  <meta property="og:type" content="article"/>
+  <script type="application/ld+json">{"@context":"https://schema.org","@type":"BlogPosting","author":{"@type":"Person","name":"Sebin George","url":"https://www.neointeraction.com/members-area/4c488cd0-f6ea-4037-9c48-8adb49ebe024/profile"},"dateModified":"2024-01-30T07:16:23.436Z","datePublished":"2024-01-24T13:11:28.411Z","headline":"UI UX Design can be enhanced using micro interactions and this is how","mainEntityOfPage":{"@id":"https://www.neointeraction.com/post/ui-ux-design-can-be-enhanced-using-micro-interactions-and-this-is-how","@type":"itemPage","url":"https://www.neointeraction.com/post/ui-ux-design-can-be-enhanced-using-micro-interactions-and-this-is-how"},"description":"Micro interactions are sole moments in product design that achieve a specific task, while boosting natural product flow.","url":"https://www.neointeraction.com/post/ui-ux-design-can-be-enhanced-using-micro-interactions-and-this-is-how","image":{"@type":"ImageObject","url":"https://static.wixstatic.com/media/c380a9_c112b0e4668d4522b99d4bc8e3d1fec6~mv2.webp/v1/fill/w_941,h_727,al_c,q_85/c380a9_c112b0e4668d4522b99d4bc8e3d1fec6~mv2.webp","width":"941","height":"727"},"publisher":{"@type":"Organization","name":"Neointeraction","url":"https://www.neointeraction.com"}}</script>
+  <meta name="google-site-verification" content="jDr4aVGcjUUHdNv8SD5bpXt6-kwDs5rDAbNqhXDrENY"/>
+  <meta property="article:author" content="Sebin George"/>
+  <meta property="article:published_time" content="2024-01-24T13:11:28.411Z"/>
+  <meta property="article:modified_time" content="2024-01-30T07:16:23.436Z"/>
+  <link rel="alternate" href="https://www.neointeraction.com/blog-feed.xml" title="Neointeraction - RSS" type="application/rss+xml"/>
+  <meta name="twitter:card" content="summary_large_image"/>
+  <meta name="twitter:title" content="UI UX Design can be enhanced using micro interactions and this is how"/>
+  <meta name="twitter:description" content="In its most basic sense, microinteractions are individual moments in the product design that accomplish a particular task, while enhancing what is intended to be the natural product flow. They are what bridge UI and UX, where functionality meets design – and they are everywhere.We as consumers see and interact with these interactive UI components daily, even without knowing. A ‘Like’ button is one of the simplest examples. A few others are: • A simple scroll bar that appears in the user interfac"/>
+  <meta name="twitter:image" content="https://static.wixstatic.com/media/c380a9_c112b0e4668d4522b99d4bc8e3d1fec6~mv2.webp/v1/fill/w_941,h_727,al_c,q_85/c380a9_c112b0e4668d4522b99d4bc8e3d1fec6~mv2.webp"/>
+   
+</head>
+
+<body>
+    <?php $page = 'blog'; ?>
+    <?php include $path.'includes/body-additional-scripts.php'; ?>
+    <!-- navbar  -->
+    <div class="navbar-container-block">
+        <?php include $path.'components/navigation.php'; ?>
+        <?php include $path.'components/whatsapp.php'; ?>
+    </div>
+    <!-- navbar -- end  -->
+    <!-- banner  -->
+    <section class="banner section-padding">
+        <div class="container">
+            <div class="blog-body-padding">
+                <p class="blog-author-text">
+                    <div class="avatar-img-block">
+                        <img src="<?php echo $path; ?>assets/images/blog/author/sebin-george.avif" alt="avatar" class="avatar-img" />
+                    </div>
+                    <span>Sebin George • 24 Jan, 2024</span>
+                </p>
+                <h1 class="banner-title">
+                    UI UX Design can be enhanced using micro interactions and this is how
+                </h1>
+                <div class="blog-banner-img">
+                    <img src="<?php echo $path; ?>assets/images/blog/ui-ux-design-microinteractions/image1.jpg" alt="blog1" />
+                </div>
+                <p class="blog-body-text">
+                    In its most basic sense, microinteractions are individual moments in the product design that accomplish a particular task, while enhancing what is intended to be the natural product flow. They are what bridge UI and UX, where functionality meets design – and they are everywhere. We as consumers see and interact with these interactive UI components daily, even without knowing. A ‘Like’ button is one of the simplest examples. A few others are:
+                </p>
+
+                <p class="blog-body-text">
+                    <ul>
+                        <li>A simple scroll bar that appears in the user interface as you scroll the mouse</li>
+                        <li>Swiping left to clear a notification on your iPhone home screen</li>
+                        <li>Ability to see that the other person is ‘typing’ on messaging apps</li>
+                        <li>A progress bar indicating the download percentage</li>
+                        <li>Pull to refresh, to reload the content on the screen of a mobile application screens</li>
+                        <li>Interactive error pages like the dinosaur game on google chrome</li>
+                    </ul><br/>
+                    Microinteractions are helpful because they provide feedback to the user by letting them know the status of an interaction, like a loading bar, for instance. Helping users see direct, real-time results of their interactions enhance the usability and the sense of direct manipulation to which users respond favorably — when they press a button, they immediately know action has been triggered through the user interface, giving a sense of control. Often, microinteractions and good UI UX design determine the platform's success.
+                </p>
+
+
+                <div class="blog-quote-block">
+                    <div class="quote-icon"></div>
+                    <p class="blog-quote-text">The best, most elegant microinteractions are often those that allow users a variety of verbs with the fewest possible nouns.- Dan Saffer</p>
+                </div>
+                
+
+                <h4 class="blog-body-title">
+                    Structure to follow for microinteractions
+                </h4>
+
+                <div class="blog-banner-img">
+                    <img src="<?php echo $path; ?>assets/images/blog/ui-ux-design-microinteractions/image2.jpg" alt="image2" />
+                </div>
+                
+
+                <p class="blog-body-text">
+                    Microinteractions work on a standard four steps. These are;
+                </p>
+
+                <p class="blog-body-text">
+                    <b>1- Trigger: </b>
+                    a trigger is the start of the micro-interactions. It can be initiated by the user or the system itself.<br/>
+                    User-initiated microinteractions can be pressing a button, swiping, scrolling, or clicking; system-initiated triggers, on the other hand, happen when certain qualifications are met, and the system decides to initiate a trigger. For example, a pop-up animation, or a notification can be system triggered. Check out the microinteractions on <a href="<?php echo $path; ?>" target="_blank">Neointeraction design</a> to understand the concept better.
+                </p>
+
+                <p class="blog-body-text">
+                    <b>2- Rules: </b>
+                    UI UX rules determine what happens after the trigger is initiated.
+                </p>
+
+                <p class="blog-body-text">
+                    <b>3- Feedback: </b>
+                    is what motion happens once the trigger is initiated, basically, everything the user hears sees, or feels (e.g. vibration motion) counts as feedback. For example, when you swipe, that is a trigger; and the animation you see on the screen (changing of the colors, highlights, or the screen in an animated way) is the feedback.
+                </p>
+
+                <p class="blog-body-text">
+                    <b>4- Loops & Modes:</b>
+                    determine the meta-rules of the microinteraction, they are needed when the conditions of the microinteractions change. They may not be a part of the initial design, but they are essential for user satisfaction.
+                </p>
+
+               
+                <h4 class="blog-body-title">
+                    Cool examples of Microinteractions
+                </h4>
+
+                <p class="blog-body-text">
+                    <ol>
+                        <li>Swipe</li>
+                        <li>Scroll</li>
+                        <li>Uploading Screens</li>
+                        <li>CTA</li>
+                        <li>Downloading Screen</li>
+                        <li>Typing</li>
+                        <li>Hovering over something</li>
+                        <li>Grayscale Switcher by Kate Laguta</li>
+                    </ol>
+                </p>
+
+
+                <div class="blog-banner-img">
+                    <img src="<?php echo $path; ?>assets/images/blog/ui-ux-design-microinteractions/image3.jpg" alt="image3" />
+                </div>
+                
+                <h4 class="blog-body-title">
+                    Salient Points
+                </h4>
+
+                <p class="blog-body-text">
+                    <ul>
+                        <li>Micro-interactions are feedback meant to communicate that something is happening based on the user's action.</li>
+                        <li>Trigger: a trigger is the start of the micro-interactions. It can be initiated by the user or the system itself.</li>
+                        <li>Rules: UI UX rules determine what motion happens after the trigger is initiated.</li>
+                        <li>Feedback: is what happens once the trigger is initiated, basically, everything the user hears sees, or feels (e.g. vibration motion) counts as feedback.</li>
+                        <li>Loops & Modes: determine the meta-rules of the microinteraction, they are needed when the conditions of the microinteractions change./li>
+                    </ul>
+                </p>
+                
+
+                <p class="blog-body-text">
+                    Get your next project designed with microinteractions to draw in your audience by hiring <a href="<?php echo $path; ?>ui-ux-design-services" target="_blank">UI UX developers and designers</a> from the Neointeraction team.
+                </p>
+
+              
+
+                <a href="<?php echo $path; ?>blogs.php" class="view-all-blogs-btn" style="text-decoration: none;">
+                    <button class="btn btn-custom btn-custom-secondary">
+                        View All Blogs
+                    </button>
+                </a>
+
+            </div>
+            <?php include $path.'components/submit-enquiry.php'; ?>
+        </div>
+
+    </section>
+    <!-- banner -- end  -->
+    <?php include $path.'components/footer.php'; ?>
+    <?php include $path.'includes/footer-additional-scripts.php'; ?>
+    <?php include $path.'includes/js.php'; ?>
+</body>
+
+</html>
