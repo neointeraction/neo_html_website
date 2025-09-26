1474ce6cb1149f535c5081f5cbe3b6d1d3ac1226
jishnu krishnan <jishnukrishnan30@gmail.com>
2025-09-26 17:30:17 +0530

    2 more projects added

--- projects/share-trading-platform.php
+++ projects/share-trading-platform.php
@@ -2,22 +2,22 @@
 <html class="no-js" lang="en">
 
 <head>
-  <title>Crew Mobility Management in a Cruise | Neointeraction Design</title>
-  <meta name="description" content="Streamline onboarding for 1,500 cruise ship employees with our new mobile solution, enhancing UX/UI to reduce operational tasks and team calls." />
-  <link rel="canonical" href="" />
-  <meta property="og:title" content="Crew Mobility Management in a Cruise | Neointeraction Design" />
-  <meta property="og:description" content="Streamline onboarding for 1,500 cruise ship employees with our new mobile solution, enhancing UX/UI to reduce operational tasks and team calls." />
-  <meta property="og:image" content="https://static.wixstatic.com/media/380122_fbdf5a539779484da4f06f782e54f0a7%7Emv2.png/v1/fit/w_2500,h_1330,al_c/380122_fbdf5a539779484da4f06f782e54f0a7%7Emv2.png" />
-  <meta property="og:image:width" content="2500" />
-  <meta property="og:image:height" content="1330" />
-  <meta property="og:url" content="" />
-  <meta property="og:site_name" content="Neointeraction Design" />
-  <meta property="og:type" content="website" />
-  <meta name="google-site-verification" content="" />
-  <meta name="twitter:title" content="Crew Mobility Management in a Cruise | Neointeraction Design" />
-  <meta name="twitter:description" content="Streamline onboarding for 1,500 cruise ship employees with our new mobile solution, enhancing UX/UI to reduce operational tasks and team calls." />
-  <meta name="twitter:image" content="https://static.wixstatic.com/media/380122_fbdf5a539779484da4f06f782e54f0a7%7Emv2.png/v1/fit/w_2500,h_1330,al_c/380122_fbdf5a539779484da4f06f782e54f0a7%7Emv2.png" />
-  <meta name="twitter:card" content="summary" />
+<title>Share Trading Platform Redesign | Neointeraction Design | Contact Now</title>
+  <meta name="description" content="Discover how Geojit BNP Paribas improved their share trading platform for web and mobile, enhancing user experience for investors and traders."/>
+  <link rel="canonical" href="https://www.neointeraction.com/share-trading-platform-redesign"/>
+  <meta property="og:title" content="Share Trading Platform Redesign | Neointeraction Design | Contact Now"/>
+  <meta property="og:description" content="Discover how Geojit BNP Paribas improved their share trading platform for web and mobile, enhancing user experience for investors and traders."/>
+  <meta property="og:image" content="https://static.wixstatic.com/media/380122_fbdf5a539779484da4f06f782e54f0a7%7Emv2.png/v1/fit/w_2500,h_1330,al_c/380122_fbdf5a539779484da4f06f782e54f0a7%7Emv2.png"/>
+  <meta property="og:image:width" content="2500"/>
+  <meta property="og:image:height" content="1330"/>
+  <meta property="og:url" content="https://www.neointeraction.com/share-trading-platform-redesign"/>
+  <meta property="og:site_name" content="Neointeraction Design"/>
+  <meta property="og:type" content="website"/>
+  <meta name="google-site-verification" content="jDr4aVGcjUUHdNv8SD5bpXt6-kwDs5rDAbNqhXDrENY"/>
+  <meta name="twitter:title" content="Share Trading Platform Redesign | Neointeraction Design | Contact Now"/>
+  <meta name="twitter:description" content="Discover how Geojit BNP Paribas improved their share trading platform for web and mobile, enhancing user experience for investors and traders."/>
+  <meta name="twitter:image" content="https://static.wixstatic.com/media/380122_fbdf5a539779484da4f06f782e54f0a7%7Emv2.png/v1/fit/w_2500,h_1330,al_c/380122_fbdf5a539779484da4f06f782e54f0a7%7Emv2.png"/>
+  <meta name="twitter:card" content="summary"/>
   <?php $path = str_repeat('../', substr_count($_SERVER['REQUEST_URI'], '/') - 2); ?>
   <?php include $path . 'includes/favicon.php'; ?>
   <?php include $path . 'includes/css.php'; ?>
@@ -45,7 +45,7 @@
     </div>
     <!-- project-banner-img -->
     <div class="project-banner-img">
-      <img src="<?php echo $path; ?>assets/images/projects/pbanner1.jpg" alt="pbanner1" />
+      <img src="<?php echo $path; ?>assets/images/projects/share-trading-platform/geogit-banner.jpg" alt="pbanner1" />
     </div>
     <div class="project-content-block">
       <div class="container">
@@ -59,7 +59,8 @@
             </div>
             <div class="col-md-9">
               <div class="quote-icon"></div>
-              <p class="blog-quote-text">A well-crafted UI powered by machine learning is not just a design; it's an evolving masterpiece that learns, adapts, and grows with the users it serves.</p>
+              <p class="blog-quote-text">Traders kept coming back to our platform and told us that they had a great trading experience with our platform whilst enjoying all the features
+              </p>
             </div>
           </div>
         </div>
@@ -71,7 +72,7 @@
               Book A Consultation
             </button>
           </div>
-          <p class="p-main-text">TM hub is an application used by the staff members aboard the Carnival ship to organize their daily activities, tasks, and information about their journey</p>
+          <p class="p-main-text">Geojit BNP Paribas revamped their trading platform, transitioning from "FLIP" to "SELFIE," for a unified and enriched user experience across devices.</p>
           <button class="btn btn-custom case-study-mobview ">
             Book A Consultation
           </button>
@@ -82,17 +83,17 @@
           <div class="row ">
             <!-- 15+ years expertise -->
             <div class="col-6 col-md-3 count-up-item">
-              <div class="count-number" data-target="90">0</div>
+              <div class="count-number" data-target="86">0</div>
               <div class="count-label">New User Engagement</div>
             </div>
             <!-- 150+ trusted clients -->
             <div class="col-6 col-md-3 count-up-item">
-              <div class="count-number" data-target="85">0</div>
+              <div class="count-number" data-target="90">0</div>
               <div class="count-label">Profits</div>
             </div>
             <!-- 80% lives impacted -->
             <div class="col-6 col-md-4 count-up-item pb-0 mt-md-0">
-              <div class="count-number" data-target="80">0</div>
+              <div class="count-number" data-target="75">0</div>
               <div class="count-label">New Customer Acquisition</div>
             </div>
 
@@ -103,70 +104,70 @@
           <div class="row">
             <div class="col-md-6">
               <h4 class="project-section-title">Objective </h4>
-              <p class="project-body-text">The aim is to enhance the daily user experience by revamping the app's interface, making it more user-friendly. The focus is on simplifying the crews' daily activities, tasks, and get information about their journey seamlessly all in one place.</p>
+              <p class="project-body-text">To improve the trading experience by creating a smart and easy-to-use platform. This platform helps traders and investors keep track of their investments and makes it simple to do research, plan strategies and giving quick access to important information.</p>
             </div>
             <div class="col-md-6">
               <h4 class="project-section-title ps-responsive-margin">Challenges </h4>
-              <p class="project-body-text">Considering the app was created by tech first approach where in it was built from the utility point of view, no significant design considerations or user research were used when creating the app for various user personas </p>
+              <p class="project-body-text">The challenges involved making it easy to place orders quickly by creating a fast and efficient way to access data. We also had to combine different services to make trading more enjoyable and come up with research and strategy features that match what different clients like. </p>
             </div>
           </div>
         </div>
         <!-- solutions-offered  -->
         <div class="solutions-offered project-section-padding">
           <h4 class="project-section-title">Solutions we offered</h4>
-          <p class="project-body-text">We revamped the app to benefit the target user by simplifying and streamlining their daily tasks, which was accomplished through,</p>
+          <p class="project-body-text">In the redesign of Geojit trading platform, SELFIE, our solutions provided traders with a personalized and all-in-one experience. We added advanced charts, included trading options, and provided detailed analytics. This helps traders make smarter decisions.</p>
 
           <div class="bs-card-section project-bs-cards">
             <div class="row">
               <!-- Card 1 -->
               <div class="col-12 col-md-6 col-lg-3">
                 <div class="custom-card">
-                  <h4 class="title">Brainstorming</h4>
+                  <h4 class="title">Personalized Trading Experience</h4>
                   <p class="description">
-                    Brainstorming sessions with the client and the creation of rough sketches served as the project's inspiration
+                  Traders can customize dashboards and widgets, tailoring their platform for individual preferences.
                   </p>
                 </div>
               </div>
               <!-- Card 2 -->
               <div class="col-12 col-md-6 col-lg-3">
                 <div class="custom-card">
-                  <h4 class="title">Identifying Key flows</h4>
+                  <h4 class="title">Unified Watchlist Look</h4>
                   <p class="description">
-                    Brainstorming sessions with the client and the creation of rough sketches served as the project's inspiration
+                  Users can enjoy a consistent watchlist experience across both web and mobile platforms for easy market monitoring.
                   </p>
                 </div>
               </div>
               <!-- Card 3 -->
               <div class="col-12 col-md-6 col-lg-3">
                 <div class="custom-card">
-                  <h4 class="title">Decision making</h4>
+                  <h4 class="title">Advanced HTML5 Charting</h4>
                   <p class="description">
-                    Presentation of the refined key flow to the stakeholders to make impactful design decisions
+                  Leverage cutting-edge charting technologies, including Renko, Kagi, P&F, and line break, for detailed market analysis.
                   </p>
                 </div>
               </div>
               <!-- Card 4 -->
               <div class="col-12 col-md-6 col-lg-3">
                 <div class="custom-card">
-                  <h4 class="title">Foundation </h4>
+                  <h4 class="title">Integrated Trading Across Assets </h4>
                   <p class="description">
-                    Wireframing , prototyping and micro interaction
+                  Users can seamlessly trade across diverse asset classes and products, enhancing trading flexibility.
                   </p>
                 </div>
               </div>
+
+              <div class="col-12 col-md-6 col-lg-3">
+                <div class="custom-card">
+                  <h4 class="title">Comprehensive F&O Analytics </h4>
+                  <p class="description">
+                  Access insightful Analytics & Visualization tools for a comprehensive understanding of futures and options markets.
+                  </p>
+                </div>
+              </div>
+
             </div>
           </div>
         </div>
-        <!-- brainstorming-session  -->
-        <div class="brainstorming-session project-section-padding">
-          <h4 class="project-section-title">Brainstorming Session </h4>
-          <p class="project-body-text">Brainstorming sessions with the client and the iterative sketches served as the project's inspiration</p>
-
-          <div class="bs-img">
-            <img src="<?php echo $path; ?>assets/images/projects/Brainstorming-Session.jpg" alt="Brainstorming-Session" />
-          </div>
-        </div>
-        <!-- solutions-help section -->
         <div class="solutions-help project-section-padding">
           <div class="title-flex">
             <h2 class="project-section-title">
@@ -177,7 +178,7 @@
             </button>
           </div>
           <p class="project-body-text">
-            Our user task-focused design empowers crew members, ensuring seamless goal achievement and enhanced productivity. Beyond technical improvements, it transforms daily routines for a more enjoyable and manageable experience.
+          The solutions streamlined user journeys and task flows, ensuring a unified experience for traders and investors. Key contributions include,
           </p>
           <button class="btn btn-custom case-study-mobview ps-responsive-margin" data-case="all" id="case-study-btn">
             Download case study
@@ -188,9 +189,9 @@
               <div
                 class="col-12 col-md-6 col-lg-3">
                 <div class="custom-card case-study-card">
-                  <h4 class="title">Improved Usability</h4>
+                  <h4 class="title">One-Page Strategy</h4>
                   <p class="description">
-                    Our design significantly enhances the system's usability, offering a more user-friendly and accessible interface.
+                  Our design approach focused on a one-page strategy, minimizing navigations for a more user-friendly experience.
                   </p>
 
                 </div>
@@ -200,9 +201,9 @@
               <div
                 class="col-12 col-md-6 col-lg-3">
                 <div class="custom-card case-study-card">
-                  <h4 class="title">User Centric UI</h4>
+                  <h4 class="title">Efficient Order Placement</h4>
                   <p class="description">
-                    Prioritizing crew well-being and mobility management, our UI design ensures both functionality and comfort.
+                  Users could easily examine and interact with portfolio and symbol information before placing orders.
                   </p>
 
                 </div>
@@ -212,35 +213,26 @@
               <div
                 class="col-12 col-md-6 col-lg-3">
                 <div class="custom-card case-study-card">
-                  <h4 class="title">Enhanced Performance</h4>
+                  <h4 class="title">Cutting-edge HTML5 Technologies</h4>
                   <p class="description">
-                    Optimized workflows and reduced processing time resulted in significant performance improvements across all modules.
+                  Post adopting the Pre-Auth feature in T1 hospitals, the daily request surged by an impressive 400%.
                   </p>
 
                 </div>
               </div>
 
               <!-- Card 4 -->
-              <div
-                class="col-12 col-md-6 col-lg-3">
-                <div class="custom-card case-study-card">
-                  <h4 class="title">Cost Efficiency</h4>
-                  <p class="description">
-                    Strategic implementation reduced operational costs while maintaining high-quality standards and user satisfaction.
-                  </p>
-
-                </div>
-              </div>
+             
             </div>
           </div>
 
         </div>
         <!-- ui-update  -->
         <div class="ui-update project-section-padding">
-          <h4 class="project-section-title">Interactive UI Upgrade </h4>
-          <p class="project-body-text">With dynamic micro interactions and illustrations, our redesign was driven by practical functionality for a more engaging and user-friendly experience.</p>
+          <h4 class="project-section-title">Dark UI Option </h4>
+          <p class="project-body-text">Recognizing users' reliance on digital devices, a dark mode option was added to reduce eye strain.</p>
           <div class="bs-img">
-            <img src="<?php echo $path; ?>assets/images/projects/ui-update.jpg" alt="ui-update" />
+            <img src="<?php echo $path; ?>assets/images/projects/share-trading-platform/geogit-mobile-ui.jpg" alt="geogit-mobile-ui" />
           </div>
         </div>
         <div class="view-all-blogs-btn">
