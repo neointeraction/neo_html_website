d27fb8988653a1a0a447729a82d565aa1173101f
jishnu krishnan <jishnukrishnan30@gmail.com>
2025-09-26 18:56:02 +0530

    added construction management project page

--- projects/construction-management-platform.php
+++ projects/construction-management-platform.php
@@ -1,10 +1,271 @@
-<?php
-  $title = "Construction Management Platform";
-  $description = "A platform for managing construction projects, from planning to execution.";
-  $image = "construction-management-platform.jpeg";
-  $alt = "Construction Management Platform";
-  $tags = ["Enterprise Apps", "B2B"];
-  $link = "construction-management-platform.php";
-  $case_study_url = "#";
-  include '../template.php';
-?>
+<!DOCTYPE html>
+<html class="no-js" lang="en">
+
+<head>
+    <title>Construction
+        Management Platform | Neointeraction Design</title>
+    <link rel="canonical" href="https://www.neointeraction.com/construction-management-platform" />
+    <meta name="robots" content="noindex" />
+    <meta property="og:title" content="Nirmaan | Neointeraction Design" />
+    <meta property="og:image"
+        content="https://static.wixstatic.com/media/380122_fbdf5a539779484da4f06f782e54f0a7%7Emv2.png/v1/fit/w_2500,h_1330,al_c/380122_fbdf5a539779484da4f06f782e54f0a7%7Emv2.png" />
+    <meta property="og:image:width" content="2500" />
+    <meta property="og:image:height" content="1330" />
+    <meta property="og:url" content="https://www.neointeraction.com/construction-management-platform" />
+    <meta property="og:site_name" content="Neointeraction Design" />
+    <meta property="og:type" content="website" />
+    <meta name="google-site-verification" content="jDr4aVGcjUUHdNv8SD5bpXt6-kwDs5rDAbNqhXDrENY" />
+    <meta name="twitter:title" content="Nirmaan | Neointeraction Design" />
+    <meta name="twitter:image"
+        content="https://static.wixstatic.com/media/380122_fbdf5a539779484da4f06f782e54f0a7%7Emv2.png/v1/fit/w_2500,h_1330,al_c/380122_fbdf5a539779484da4f06f782e54f0a7%7Emv2.png" />
+    <meta name="twitter:card" content="summary" />
+    <?php $path = str_repeat('../', substr_count($_SERVER['REQUEST_URI'], '/') - 2); ?>
+    <?php include $path . 'includes/favicon.php'; ?>
+    <?php include $path . 'includes/css.php'; ?>
+    <?php include $path . 'includes/head-additional-scripts.php'; ?>
+</head>
+
+<body>
+    <?php
+  $page = 'Projects';
+  include $path . 'includes/case_study_helper.php';
+  ?>
+    <?php include $path . 'includes/body-additional-scripts.php'; ?>
+    <!-- navbar  -->
+    <div class="navbar-container-block">
+        <?php include $path . 'components/navigation.php'; ?>
+        <?php include $path . 'components/whatsapp.php'; ?>
+    </div>
+    <!-- navbar -- end  -->
+    <!-- banner  -->
+    <section class="banner section-padding">
+        <div class="container">
+            <h1 class="banner-title">
+                Construction
+                Management Platform
+            </h1>
+        </div>
+        <!-- project-banner-img -->
+        <div class="project-banner-img">
+            <img src="<?php echo $path; ?>assets/images/projects/construction-management/construction-management-banner.jpg"
+                alt="construction-management" />
+        </div>
+        <div class="project-content-block">
+            <div class="container">
+                <!-- blog-quote-block  -->
+                <!-- <div class="blog-quote-block project-quote-block">
+          <div class="row justify-content-center align-items-center">
+            <div class="col-md-3">
+              <div class="p-logo">
+                <img src="<?php echo $path; ?>assets/images/projects/ai-driven-healthcare-consultation-platform/tricog.png" alt="tricog" />
+              </div>
+            </div>
+            <div class="col-md-9">
+              <div class="quote-icon"></div>
+              <p class="blog-quote-text">We aimed to make MEGA We Care’s corporate website more user-centric and engaging, with a focus on reducing bounce rates and improving return visits. The outcome reflected a noticeable improvement in user interaction and alignment with business goals.</p>
+            </div>
+          </div>
+        </div> -->
+                <!-- overview-block -->
+                <div class="overview-block project-section-padding">
+                    <div class="title-flex">
+                        <h4 class="project-section-title">Overview </h4>
+                        <button class="btn btn-custom case-study-webview ">
+                            Book A Consultation
+                        </button>
+                    </div>
+                    <p class="p-main-text">The project management platform simplifies project scheduling, WBS, and resource management, ensuring seamless integration with ACC and local files.</p>
+                    <button class="btn btn-custom case-study-mobview ">
+                        Book A Consultation
+                    </button>
+                </div>
+                <!-- result-block -->
+
+                <!-- objective-challenges  -->
+                <div class="objective-challenges project-section-padding">
+                    <div class="row">
+                        <div class="col-md-6">
+                            <h4 class="project-section-title">Objective </h4>
+                            <p class="project-body-text">The goal was to design a seamless, user-friendly website for
+                            The primary goal of this User Experience Design project was to enhance usability and efficiency for construction professionals. The platform required an intuitive User Interface Design to help users configure projects, manage dependencies, and optimize schedules. A key challenge was designing a system that simplifies complex workflows while ensuring project managers and engineers can easily navigate and manage tasks.</p>
+                        </div>
+                        <div class="col-md-6">
+                            <h4 class="project-section-title ps-responsive-margin">Challenges </h4>
+                            <p class="project-body-text">One of the biggest challenges was designing an intuitive UX research-backed solution for users with varying technical expertise. The platform needed a balance between manual control and automation to streamline project structuring, scheduling, and dependencies. The existing solutions lacked a seamless Wireframing and Prototyping approach, making it difficult for users to visualize project phases. Additionally, Visual Design inconsistencies in dashboards led to usability issues, requiring a UI Design overhaul for clarity and efficiency.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <!-- solutions-offered  -->
+                <div class="solutions-offered project-section-padding">
+                    <h4 class="project-section-title">Solutions we offered</h4>
+                    <p class="project-body-text">Our approach streamlined project scheduling, WBS structuring, and resource management for a seamless construction workflow.</p>
+
+
+                    <div class="bs-card-section project-bs-cards">
+                        <div class="row">
+                            <!-- Card 1 -->
+                            <div class="col-12 col-md-6 col-lg-3">
+                                <div class="custom-card">
+                                    <h4 class="title">Enhanced Role-Based Access</h4>
+                                    <p class="description">
+                                    Designed intuitive dashboards tailored for different user roles, ensuring clear task management.
+                                    </p>
+                                </div>
+                            </div>
+                            <!-- Card 2 -->
+                            <div class="col-12 col-md-6 col-lg-3">
+                                <div class="custom-card">
+                                    <h4 class="title">Optimizing Information Architecture</h4>
+                                    <p class="description">
+                                    Reorganized navigation for simplified access to project timelines, resources, and dependencies.
+                                    </p>
+                                </div>
+                            </div>
+
+                            <div class="col-12 col-md-6 col-lg-3">
+                                <div class="custom-card">
+                                    <h4 class="title">
+                                    Refined UI & Workflows</h4>
+                                    <p class="description">
+                                    Improved usability, clarity, and engagement with a structured UX Design approach.
+                                    </p>
+                                </div>
+                            </div>
+
+                            <div class="col-12 col-md-6 col-lg-3">
+                                <div class="custom-card">
+                                    <h4 class="title">
+                                    Customization for Project Needs</h4>
+                                    <p class="description">
+                                    Enabled flexible file integration, supporting both ACC and local file uploads for project configuration.
+                                    </p>
+                                </div>
+                            </div>
+
+
+                        </div>
+                    </div>
+                </div>
+
+    
+
+                <!-- solutions-help section -->
+                <div class="solutions-help project-section-padding">
+                    <div class="title-flex">
+                        <h2 class="project-section-title">
+                            How did our solutions help?
+                        </h2>
+                    </div>
+                    <p class="project-body-text">
+                    Our approach ensured a seamless experience for both employees and managers while optimizing construction management.
+                    </p>
+                    <div class="bs-card-section project-bs-cards">
+                        <div class="row justify-content-center">
+                            <!-- Card 1 -->
+                            <div class="col-12 col-md-6">
+                                <div class="custom-card case-study-card">
+                                    <h4 class="title">Streamlining Project Structuring</h4>
+                                    <p class="description">
+                                    We designed a structured WBS and scheduling system, reducing the complexity of project planning and execution.
+                                    </p>
+                                </div>
+                            </div>
+                            <!-- Card 2 -->
+                            <div class="col-12 col-md-6">
+                                <div class="custom-card case-study-card">
+                                    <h4 class="title">Optimizing Information Architecture</h4>
+                                    <p class="description">
+                                    By restructuring navigation, we made it easier for users to access project data, schedules, and dependencies with minimal complexity.
+                                    </p>
+                                </div>
+                            </div>
+
+                            <div class="col-12 col-md-6">
+                                <div class="custom-card case-study-card">
+                                    <h4 class="title">Redesigning the UI & Workflows</h4>
+                                    <p class="description">
+                                    A refreshed UI Design improved workflows, enhancing usability, clarity, and efficiency for project managers and site engineers.
+                                    </p>
+                                </div>
+                            </div>
+
+                            <div class="col-12 col-md-6">
+                                <div class="custom-card case-study-card">
+                                    <h4 class="title">Customization for Diverse Project Needs</h4>
+                                    <p class="description">
+                                    Enabled seamless integration with ACC while allowing local file imports for flexibility in project configurations.
+                                    </p>
+                                </div>
+                            </div>
+
+
+                        </div>
+                    </div>
+                </div>
+                <!-- ui-update  -->
+                <div class="ui-update project-section-padding">
+
+                    <h4 class="project-section-title">Our Approach </h4>
+                    <p class="project-body-text">Our process began with User Experience Design, conducting UX research through industry expert interviews, competitor analysis, and usability testing. We identified pain points in existing project management tools and optimized Information Architecture for construction workflows.</p>
+                    <p class="project-body-text">We then moved to moodboarding and Visual Design, iterating on layouts, color schemes, and dashboard aesthetics to create a cohesive UI system.</p>
+                    <p class="project-body-text">In the Wireframing phase, we mapped out user flows with low-fidelity wireframes, refining them into high-fidelity UI designs through multiple iterations. Finally, during the Prototyping and development handoff, we provided structured documentation, interactive prototypes, and regular design reviews to ensure seamless implementation.</p>
+                    <div class="bs-img">
+                        <img src="<?php echo $path; ?>assets/images/projects/construction-management/construction-management-website-ui.jpg"
+                            alt="construction-management-website-ui" />
+                    </div>
+                </div>
+                <div class="view-all-blogs-btn">
+                    <button class="btn btn-custom btn-custom-secondary"
+                        onclick="window.location.href='<?php echo $path; ?>projects.php'">
+                        View More Projects
+                    </button>
+                </div>
+                <?php include 'components/submit-enquiry.php'; ?>
+            </div>
+        </div>
+    </section>
+
+
+
+
+    <!-- Case Study Download Modal -->
+    <div class="modal fade" id="caseStudyModal" tabindex="-1" role="dialog" aria-labelledby="caseStudyModalLabel"
+        aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="caseStudyModalLabel">Download Case Study</h5>
+                    <!-- Change this line in your modal -->
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <p class="mb-3 mt-3">Enter your email to download the case study and stay updated with our latest
+                        insights.</p>
+                    <form id="caseStudyForm">
+                        <div class="mb-3">
+                            <input type="email" id="case-study-email" class="form-control"
+                                placeholder="Enter your work email" required>
+                        </div>
+                        <div id="case-study-status" class="mb-3 text-center"></div>
+                        <div class="d-flex justify-content-center">
+                            <button type="submit" class="btn btn-custom" id="case-study-submit">
+                                Download Case Study
+                            </button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    <?php include $path . 'components/projects-enquiry.php'; ?>
+    <?php include $path . 'components/registration-modal.php'; ?>
+    <?php include $path . 'components/download-casestudy-modal.php'; ?>
+    <?php include $path . 'components/footer.php'; ?>
+    <?php include $path . 'includes/footer-additional-scripts.php'; ?>
+    <?php include $path . 'includes/js.php'; ?>
+</body>
+
+</html>
