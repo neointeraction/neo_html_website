383056eeb4ec57c2eefd2492754f09a6b3656e7f
vmidhun <vmidhun@gmail.com>
2025-10-14 10:10:55 +0530

    updated file zomata

--- blogs/how-zomato-s-gamification-took-the-2023-world-cup-with-a-cricket-twist.php
+++ blogs/how-zomato-s-gamification-took-the-2023-world-cup-with-a-cricket-twist.php
@@ -6,26 +6,25 @@
   <?php include $path.'includes/favicon.php'; ?>
   <?php include $path.'includes/css.php'; ?>
   <?php include $path.'includes/head-additional-scripts.php'; ?>
-  <title>Effective UX Audit and Website Redesign to boost UX for LIC</title>
-  <meta name="description" content="Design case study on Effective UX Audit and Redesigning LIC Website to boos user experience"/>
-  <link rel="canonical" href="https://www.neointeraction.com/post/effective-ux-audit-and-website-redesign-to-boost-ux-for-lic"/>
-  <meta property="og:title" content="Effective UX Audit and Website Redesign to boost UX for LIC"/>
-  <meta property="og:description" content="Life Insurance of Corporation is a government-owned insurance firm in India. It is the largest insurance firm in the region, having a vast number of policyholders and agents. With many businesses going online, websites have become a big deal for all large corporations. And unfortunately, the current LIC website design appears to be out of date. The primary goal of the website is to provide a one-stop online solution to policyholders and insurance advisors, But the site's biggest flaw is the lack"/>
-  <meta property="og:image" content="https://static.wixstatic.com/media/c380a9_ba33f154326d4c51a38b3c71d85f6529~mv2.webp/v1/fill/w_941,h_727,al_c,q_85/c380a9_ba33f154326d4c51a38b3c71d85f6529~mv2.webp"/>
-  <meta property="og:image:width" content="941"/>
-  <meta property="og:image:height" content="727"/>
-  <meta property="og:url" content="https://www.neointeraction.com/post/effective-ux-audit-and-website-redesign-to-boost-ux-for-lic"/>
+  <title>How Zomato's Gamification Took the 2023 World Cup with a Cricket Twist</title>
+  <meta name="description" content="Zomato leveraged gamification during the 2023 Cricket World Cup to enhance user engagement. This case study explores how they successfully combined food delivery with live cricket updates."/>
+  <link rel="canonical" href="https://www.neointeraction.com/blogs/how-zomato-s-gamification-took-the-2023-world-cup-with-a-cricket-twist.php"/>
+  <meta property="og:title" content="How Zomato's Gamification Took the 2023 World Cup with a Cricket Twist"/>
+  <meta property="og:description" content="Zomato leveraged gamification during the 2023 Cricket World Cup to enhance user engagement. This case study explores how they successfully combined food delivery with live cricket updates."/>
+  <meta property="og:image" content="https://static.wixstatic.com/media/c16c07_64bfc434c3404a0188903f89d97c061d~mv2.webp/v1/fill/w_1252,h_968,al_c,q_85,usm_0.66_1.00_0.01,enc_avif,quality_auto/c16c07_64bfc434c3404a0188903f89d97c061d~mv2.webp"/>
+  <meta property="og:image:width" content="1252"/>
+  <meta property="og:image:height" content="968"/>
+  <meta property="og:url" content="https://www.neointeraction.com/blogs/how-zomato-s-gamification-took-the-2023-world-cup-with-a-cricket-twist.php"/>
   <meta property="og:site_name" content="Neointeraction"/>
   <meta property="og:type" content="article"/>
-  <meta name="google-site-verification" content="jDr4aVGcjUUHdNv8SD5bpXt6-kwDs5rDAbNqhXDrENY"/>
-  <meta property="article:author" content="Thirunavukkarasu Lakshumanan"/>
-  <meta property="article:published_time" content="2024-01-25T10:58:02.450Z"/>
-  <meta property="article:modified_time" content="2024-08-23T04:54:56.696Z"/>
+  <meta property="article:author" content="Veena Varghese"/>
+  <meta property="article:published_time" content="2024-04-22T10:58:02.450Z"/>
+  <meta property="article:modified_time" content="2024-04-22T10:58:02.450Z"/>
   <link rel="alternate" href="https://www.neointeraction.com/blog-feed.xml" title="Neointeraction - RSS" type="application/rss+xml"/>
   <meta name="twitter:card" content="summary_large_image"/>
-  <meta name="twitter:title" content="Effective UX Audit and Website Redesign to boost UX for LIC"/>
-  <meta name="twitter:description" content="Life Insurance of Corporation is a government-owned insurance firm in India. It is the largest insurance firm in the region, having a vast number of policyholders and agents. With many businesses going online, websites have become a big deal for all large corporations. And unfortunately, the current LIC website design appears to be out of date. The primary goal of the website is to provide a one-stop online solution to policyholders and insurance advisors, But the site's biggest flaw is the lack"/>
-  <meta name="twitter:image" content="https://static.wixstatic.com/media/c380a9_ba33f154326d4c51a38b3c71d85f6529~mv2.webp/v1/fill/w_941,h_727,al_c,q_85/c380a9_ba33f154326d4c51a38b3c71d85f6529~mv2.webp"/>
+  <meta name="twitter:title" content="How Zomato's Gamification Took the 2023 World Cup with a Cricket Twist"/>
+  <meta name="twitter:description" content="Zomato leveraged gamification during the 2023 Cricket World Cup to enhance user engagement. This case study explores how they successfully combined food delivery with live cricket updates."/>
+  <meta name="twitter:image" content="https://static.wixstatic.com/media/c16c07_64bfc434c3404a0188903f89d97c061d~mv2.webp/v1/fill/w_1252,h_968,al_c,q_85,usm_0.66_1.00_0.01,enc_avif,quality_auto/c16c07_64bfc434c3404a0188903f89d97c061d~mv2.webp"/>
    
 </head>
 
@@ -42,63 +41,65 @@
     <section class="banner section-padding">
         <div class="container">
             <div class="blog-body-padding">
-                <p class="blog-author-text">
+                 <p class="blog-author-text">
                     <div class="avatar-img-block">
-                        <img src="<?php echo $path; ?>assets/images/blog/author/.png" alt="avatar" class="avatar-img" />
+                        <img src="<?php echo $path; ?>assets/images/blog/author/veena-varghese.avif" alt="avatar" class="avatar-img" />
                     </div>
-                    <span> • 25 Jan, 2024</span>
+                    <span>Veena Varghese • 11 Dec, 2024</span>
                 </p>
-                <h1 class="banner-title">
-                Effective UX Audit and Website Redesign to boost UX for LIC
-                </h1>
+                <h1 class="blog-title">How Zomato's Gamification Took the 2023 World Cup with a Cricket Twist</h1>
+                
                 <div class="blog-banner-img">
-                    <img src="<?php echo $path; ?>assets/images/blog/effective-ux-audit/image1.jpg" alt="blog1" />
+                    <img src="https://static.wixstatic.com/media/c16c07_64bfc434c3404a0188903f89d97c061d~mv2.webp/v1/fill/w_1252,h_968,al_c,q_85,usm_0.66_1.00_0.01,enc_avif,quality_auto/c16c07_64bfc434c3404a0188903f89d97c061d~mv2.webp" alt="Zomato Cricket World Cup Gamification" />
                 </div>
-                <p class="blog-body-text">
-                    Life Insurance of Corporation is a government-owned insurance firm in India. It is the largest insurance firm in the region, having a vast number of policyholders and agents. With many businesses going online, websites have become a big deal for all large corporations. And unfortunately, the current LIC website design appears to be out of date. The primary goal of the website is to provide a one-stop online solution to policyholders and insurance advisors, But the site's biggest flaw is the lack of a good user experience. As one of the top ui ux design companies we truly enjoyed redesigning the ux audit for LIC.
-                </p>
-                <h4 class="blog-body-title">
-                    Identified Problems
-                </h4>
 
-                <p class="blog-body-text">
-                    <b>Problems related to the UX:</b>
-                    <ul>
-                        <li>The placement of the banner was unclear.</li>
-                        <li>There are too many undesired options on the navigation bar.</li>
-                        <li>Although paying premiums is the most common option for policyholders on the LIC website, it was not prominently shown on the landing page.</li>
-                        <li>Some key items, such as Buy Insurance, Renew Policy, Claim Policy, Claim status, and Premium Calculator, are not highlighted and are difficult to locate.</li>
-                        <li>Users' download links for mobile apps were misplaced.</li>
-                        <li>The landing page is clogged with unnecessary information and content.</li>
-                        <li>The footer options are cluttered and unorganized.</li>
-                    </ul>
-                </p>
+                <p class="blog-body-text">In the dynamic world of the online food delivery industry, companies are continually seeking innovative ways to capture consumer attention and stand out amidst the crowd. Major events and festivals serve as prime opportunities for brands to enhance their marketing strategies and solidify their presence in the market. These occasions offer a unique platform for companies to connect with their audience on a deeper level, tapping into the excitement and energy surrounding these events.</p>
+                <p class="blog-body-text">One company that stood out during the Cricket World Cup 2023 was Zomato, by cleverly using gamification and cricket to enhance user engagement. Through a partnership with Roanuz Sports Data, a leading provider of sports data solutions, Zomato introduced a gamified food ordering experience that appealed to both cricket fans and food lovers. Let's explore how Zomato achieved success with this innovative user experience.</p>
+                
+                <h4 class="blog-body-title">World Cup 2023 Campaign</h4>
+                <p class="blog-body-text">Cricket is more than just a sport in India; it's a passion that unites millions of people. Similarly, food holds a special place in people's hearts, especially when it comes to enjoying snacks while watching a thrilling cricket match. Recognizing this intersection, Zomato saw an opportunity to combine the two and create an immersive experience for users during the Cricket World Cup.</p>
+                <p class="blog-body-text">Zomato's Cricket World Cup 2023 campaign was a masterclass in leveraging gamification to enhance user engagement. The company partnered with Roanuz Sports Data, a leading provider of sports-related data and analytics, to integrate real-time cricket updates and statistics into their app. Users were not only able to track live scores and match updates but also participate in cricket-themed challenges and games while placing their food orders.</p>
+
+                <div class="blog-banner-img">
+                    <img src="https://static.wixstatic.com/media/c16c07_7cebadb2e5df49159485d8adc2105e1a~mv2.webp/v1/fill/w_1252,h_968,al_c,q_85,usm_0.66_1.00_0.01,enc_avif,quality_auto/c16c07_7cebadb2e5df49159485d8adc2105e1a~mv2.webp" alt="Zomato Cricket Campaign" />
+                </div>
+
+                <h4 class="blog-body-title">What is Gamification?</h4>
+                <p class="blog-body-text"><a href="https://www.neointeraction.com/post/turning-saving-into-a-game-gamified-design-approaches-to-encourage-financial-discipline" target="_blank">Gamification</a> has become a popular strategy for businesses to increase user engagement and loyalty. By incorporating game-like elements such as challenges, rewards, and competitions into their platform, companies can make mundane tasks more enjoyable and rewarding. Zomato took this concept and applied it to their food delivery platform, turning ordering meals into a fun and interactive experience.</p>
+
+                <h4 class="blog-body-title">Execution and Role of Roanuz Sports Data</h4>
+                <p class="blog-body-text">Zomato showcased its innovative approach to engaging users by adding cricket-themed gamification into its platform:</p>
+                <ul>
+                    <li><strong>Cricket Section Highlights:</strong> Zomato's app featured a dedicated section highlighting ongoing matches and upcoming fixtures during the Cricket World Cup.</li>
+                    <li><strong>Prediction Features:</strong> Users could access match schedules, player statistics, and even predict match outcomes to earn rewards.</li>
+                    <li><strong>Milestone based Discounts:</strong> Exclusive deals and discounts were offered based on specific cricketing milestones, such as discounts on biryani for centuries scored by the Indian cricket team.</li>
+                    <li><strong>Integration of Roanuz Sports Data:</strong> Roanuz Sports Data provided real-time sports data through its comprehensive API, allowing Zomato to access live scores, player stats, and match insights.</li>
+                </ul>
 
                 <div class="blog-quote-block">
                     <div class="quote-icon"></div>
-                    <p class="blog-quote-text">The only constant in the world is “CHANGE". Though this line has become a cliche, it is very important to implement the same in our operation. And of course, the change needs to be planned well considering all the right and critical factors.</p>
+                    <p class="blog-quote-text">The partnership with Roanuz Sports Data allowed us to create a truly immersive experience for cricket fans, bringing real-time match updates and statistics directly to their fingertips.<br>- Priyanka Sahay, Head of Marketing at Zomato</p>
                 </div>
 
-               
-                <p class="blog-body-text">
-                    <a href="" target="_blank">here</a>.
-                </p>
+                <h4 class="blog-body-title">Results</h4>
+                <p class="blog-body-text">Zomato's Cricket World Cup 2023 campaign marked a significant success, setting a new benchmark for engagement in the food tech industry.</p>
+                <p class="blog-body-text"><strong>1. Active Users and Engagement:</strong> Zomato's gamified experience during the Cricket World Cup 2023 attracted millions of active users. According to a report by <a href="https://www.businessinsider.in/business/startups/news/checkout-zomato-top-executives-salaries-including-co-founder-and-ceo-deepinder-goyal/slidelist/82289352.cms" target="_blank"> Business Insider India</a>, Zomato's CEO Deepinder Goyal stated that the campaign saw "tens of millions of users actively participate in the gamified experience."</p>
+                <p class="blog-body-text"><strong>2. Initial Success During IPL Season:</strong> The feature's success was evident during its initial launch in the 2019 IPL season. According to the statistics reported by the Economic Times, within a week of its introduction, it garnered over 120,000 active users and generated more than 4 million predictions in just three weeks.</p>
+                <p class="blog-body-text"><strong>3. Increase in App Downloads and Order Volumes:</strong> Zomato observed a significant increase in app downloads, order volumes, and user retention during the Cricket World Cup 2023 period. According to Zomato's official blog post, app downloads surged by 20%, while order volumes increased by 18%, indicating the campaign's effectiveness in driving user engagement and transactions.</p>
+                <p class="blog-body-text"><strong>4. Impact on Industry Standards:</strong> Zomato's innovative approach to merging cricket and food delivery set a new standard for engagement in the food tech industry. This was acknowledged by various industry analysts and experts. For instance, in an article by Livemint, industry experts hailed Zomato's campaign as a game-changer, illustrating its influence on industry practices and customer engagement strategies.</p>
 
-               
+                <div class="blog-banner-img">
+                    <img src="https://static.wixstatic.com/media/c16c07_31c7eac29aab4d4d94f7e0d212c45095~mv2.webp" alt="Zomato Gamification Results" />
+                </div>
 
                 <h4 class="blog-body-title">Conclusion</h4>
-
-                <p class="blog-body-text">
-                    UX Audit is a necessary aspect of the design process in order to create a solution that meets both business and user requirements. If we focus on the details of the product and services, we tend to overlook the users and their perspectives. UX strategy and research play an important role in the process of product design. The most important factor of this project was that users might easily be lost due to a bad online presence experience.
-                </p>
-
+                <p class="blog-body-text">Retaining users is one of the greatest challenges faced by businesses but Zomato's gamification efforts during the Cricket World Cup 2023 showcased the potential of merging sports, technology, and food delivery. As businesses increasingly realize the crucial role of user experience in driving success, collaborating with UX design agencies like <a href="https://www.neointeraction.com/" target="_blank"> Neointeraction Design</a> become indispensable in shaping the future of customer engagement and loyalty in online commerce. Neointeraction Design brings a creative touch to digital platforms, seamlessly integrating top-notch <a href="https://www.neointeraction.com/ui-ux-design-developer-services-company-bangalore" target="_blank">UI UX design services</a> that will help market your product better, thereby contributing significantly to business growth.</p>
 
                 <a href="<?php echo $path; ?>blogs.php" class="view-all-blogs-btn" style="text-decoration: none;">
                     <button class="btn btn-custom btn-custom-secondary">
                         View All Blogs
                     </button>
                 </a>
-
             </div>
             <?php include $path.'components/submit-enquiry.php'; ?>
         </div>
