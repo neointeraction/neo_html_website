65b165efb3dfa07ce6db715deb3f25d31d7b08ab
jishnu krishnan <jishnukrishnan30@gmail.com>
2025-10-14 13:25:14 +0530

    images corrected for how to improve

--- blogs/how-to-improve-your-digital-content-using-appropriate-typography.php
+++ blogs/how-to-improve-your-digital-content-using-appropriate-typography.php
@@ -11,7 +11,7 @@
   <link rel="canonical" href="https://www.neointeraction.com/blogs/how-to-improve-your-digital-content-using-appropriate-typography.php"/>
   <meta property="og:title" content="How to improve your digital content using appropriate typography?"/>
   <meta property="og:description" content="Explore the power of typography in digital content. Learn how choosing the right fonts, sizes, and colors can enhance user experience, build brand recognition, and improve readability."/>
-  <meta property="og:image" content="https://www.neointeraction.com/assets/images/blog/digital-content-typography/typography-social-media-posts.webp"/>
+  <meta property="og:image" content="<?php echo $path; ?>assets/images/blog/digital-content-typography/digital-typography-tips-thumbnail-mobile.jpeg"/>
   <meta property="og:image:width" content="941"/>
   <meta property="og:image:height" content="941"/>
   <meta property="og:url" content="https://www.neointeraction.com/blogs/how-to-improve-your-digital-content-using-appropriate-typography.php"/>
@@ -25,7 +25,7 @@
   <meta name="twitter:card" content="summary_large_image"/>
   <meta name="twitter:title" content="How to improve your digital content using appropriate typography?"/>
   <meta name="twitter:description" content="Explore the power of typography in digital content. Learn how choosing the right fonts, sizes, and colors can enhance user experience, build brand recognition, and improve readability."/>
-  <meta name="twitter:image" content="https://www.neointeraction.com/assets/images/blog/digital-content-typography/typography-social-media-posts.webp"/>
+  <meta name="twitter:image" content="<?php echo $path; ?>assets/images/blog/digital-content-typography/digital-typography-tips-thumbnail-mobile.jpeg"/>
    
 </head>
 
@@ -44,19 +44,19 @@
             <div class="blog-body-padding">
      <p class="blog-author-text">
                     <div class="avatar-img-block">
-                        <img src="<?php echo $path; ?>assets/images/blog/author/ashutosh-shakya.webp" alt="avatar" class="avatar-img" />
+                        <img src="<?php echo $path; ?>assets/images/blog/author/ashutosh-shakya.avif" alt="avatar" class="avatar-img" />
                     </div>
                     <span>Ashutosh Shakya • 24 Jan, 2024</span>
                 </p>
     <h1 class="blog-title">How to improve your digital content using appropriate typography?</h1>
     
     <div class="blog-banner-img">
-        <img src="<?php echo $path; ?>assets/images/blog/digital-content-typography/typography-social-media-posts.webp" alt="typography social media posts" />
+        <img src="<?php echo $path; ?>assets/images/blog/digital-content-typography/digital-typography-tips.jpeg" alt="typography social media posts" />
     </div>
     <p class="blog-body-text">When you create content, one thing you must take care of is the balance of graphics and text. This is where typography comes in as it attracts the user to read the text. Typography is mainly customizing the text according to the design to make it aesthetically pleasing. Also, it sets the tone of the design and ensures a great <a href="https://www.neointeraction.com/ui-ux-design-developer-services-company-bangalore#:~:text=Contact%20Us-,UX%20Design,-Design%20to%20shape" target="_blank">user experience</a>.</p>
     <h4 class="blog-body-title">Why is typography important?</h4>
     <div class="blog-banner-img">
-        <img src="<?php echo $path; ?>assets/images/blog/digital-content-typography/importance-typography.webp" alt="importance typography" />
+        <img src="<?php echo $path; ?>assets/images/blog/digital-content-typography/typography-importance.jpeg" alt="importance typography" />
     </div>
     <p class="blog-body-text">A website's graphical balance, a strong visual hierarchy, and the overall tone of the product can all be established with good typography. Typography should guide and enlighten your users, improve readability and accessibility, and provide a pleasant user experience.</p>
     <ul>
@@ -69,7 +69,7 @@
     </div>
     <h4 class="blog-body-title">Typography in Digital Media</h4>
     <div class="blog-banner-img">
-        <img src="<?php echo $path; ?>assets/images/blog/digital-content-typography/typography-emphasis.webp" alt="typography emphasis" />
+        <img src="<?php echo $path; ?>assets/images/blog/digital-content-typography/digital-typography.jpeg" alt="typography emphasis" />
     </div>
     <p class="blog-body-text">The most popular methods for achieving emphasis in digital media are comparable to those used in print, but with one key difference: on-screen typography typically has a fluid appearance rather than a fixed appearance. Due to variations in scale, resolution, and settings across multiple devices, it can vary from viewer to viewer.</p>
     <p class="blog-body-text">Some of the most popular methods for emphasizing content in digital media include:</p>
@@ -83,7 +83,7 @@
     <p class="blog-body-text">The <a href="https://www.neointeraction.com/post/the-impact-of-color-in-branding" target="_blank">use of color</a> can achieve eye-catching emphasis. It improves the overall look of the page and attracts attention to the type without changing the typestyle or any other formatting.</p>
     <h4 class="blog-body-title">Important aspects of Typography</h4>
     <div class="blog-banner-img">
-        <img src="<?php echo $path; ?>assets/images/blog/digital-content-typography/aspects-typography.webp" alt="aspects typography" />
+        <img src="<?php echo $path; ?>assets/images/blog/digital-content-typography/typography-aspects.jpeg" alt="aspects typography" />
     </div>
     <p class="blog-body-text">These aspects help to make written language legible, readable and appealing to the user. It plays a crucial role in shaping the user experience and can greatly impact the way users engage with and understand your content.</p>
     <p class="blog-body-text"><strong>Choosing a font:</strong></p>
@@ -94,7 +94,7 @@
     <p class="blog-body-text">The color of the font is also an important aspect of typography. Black or dark gray fonts are the most legible and easiest to read on light backgrounds. If you decide to use a color font, it's important to ensure that there is enough contrast between the font color and the background color. This is especially important for users with visual impairments, as low contrast can make text difficult to read. The Web Content Accessibility Guidelines (WCAG) recommend a contrast ratio of at least 4.5:1 for body text and 3:1 for large text.</p>
     <h4 class="blog-body-title">Typography rules to follow</h4>
     <div class="blog-banner-img">
-        <img src="<?php echo $path; ?>assets/images/blog/digital-content-typography/typography-rules.webp" alt="typography rules" />
+        <img src="<?php echo $path; ?>assets/images/blog/digital-content-typography/typography-rules.jpeg" alt="typography rules" />
     </div>
     <ul>
         <li>Keep the number of fonts to a minimum. As long as there is coherence in the pairing, don't be afraid to use fonts from other typeface families.</li>
@@ -107,7 +107,7 @@
     </ul>
     <h4 class="blog-body-title">Takeaway</h4>
     <div class="blog-banner-img">
-        <img src="<?php echo $path; ?>assets/images/blog/digital-content-typography/typography-resources.webp" alt="typography resources" />
+        <img src="<?php echo $path; ?>assets/images/blog/digital-content-typography/typography-takeaways.jpeg" alt="typography resources" />
     </div>
     <p class="blog-body-text">Typography is frequently underestimated, but it is an essential component of UI UX design. If you need help upgrading your product experience through aesthetically pleasing and functional design, connect with our ux ui design service team at <a href="https://www.neointeraction.com/" target="_blank">Neointeraction design</a>.</p>
     <h4 class="blog-body-title">Design Resources For Typography</h4>
