dbfe2463b8698b322051a7263920b4b61ff3675a
vmidhun <vmidhun@gmail.com>
2025-10-14 18:59:17 +0530

    updating blog data

--- blogs/ux-design-guidelines-that-must-be-followed-while-designing-a-mobile-app.php
+++ blogs/ux-design-guidelines-that-must-be-followed-while-designing-a-mobile-app.php
@@ -6,26 +6,26 @@
   <?php include $path.'includes/favicon.php'; ?>
   <?php include $path.'includes/css.php'; ?>
   <?php include $path.'includes/head-additional-scripts.php'; ?>
-  <title>UX Design Guidelines for Mobile App Design</title>
-  <meta name="description" content="Discover the essential UX design guidelines that every mobile app designer must follow to create intuitive, user-friendly, and successful mobile applications."/>
-  <link rel="canonical" href="https://www.neointeraction.com/blogs/ux-design-guidelines-that-must-be-followed-while-designing-a-mobile-app.php"/>
-  <meta property="og:title" content="UX Design Guidelines for Mobile App Design"/>
-  <meta property="og:description" content="Discover the essential UX design guidelines that every mobile app designer must follow to create intuitive, user-friendly, and successful mobile applications."/>
-  <meta property="og:image" content="https://www.neointeraction.com/assets/images/blog/ux-design-guidelines-mobile-app/mobie-app-design_guidelines.jpeg"/>
+  <title>Effective UX Audit and Website Redesign to boost UX for LIC</title>
+  <meta name="description" content="Design case study on Effective UX Audit and Redesigning LIC Website to boos user experience"/>
+  <link rel="canonical" href="https://www.neointeraction.com/post/effective-ux-audit-and-website-redesign-to-boost-ux-for-lic"/>
+  <meta property="og:title" content="Effective UX Audit and Website Redesign to boost UX for LIC"/>
+  <meta property="og:description" content="Life Insurance of Corporation is a government-owned insurance firm in India. It is the largest insurance firm in the region, having a vast number of policyholders and agents. With many businesses going online, websites have become a big deal for all large corporations. And unfortunately, the current LIC website design appears to be out of date. The primary goal of the website is to provide a one-stop online solution to policyholders and insurance advisors, But the site's biggest flaw is the lack"/>
+  <meta property="og:image" content="https://static.wixstatic.com/media/c380a9_ba33f154326d4c51a38b3c71d85f6529~mv2.webp/v1/fill/w_941,h_727,al_c,q_85/c380a9_ba33f154326d4c51a38b3c71d85f6529~mv2.webp"/>
   <meta property="og:image:width" content="941"/>
   <meta property="og:image:height" content="727"/>
-  <meta property="og:url" content="https://www.neointeraction.com/blogs/ux-design-guidelines-that-must-be-followed-while-designing-a-mobile-app.php"/>
+  <meta property="og:url" content="https://www.neointeraction.com/post/effective-ux-audit-and-website-redesign-to-boost-ux-for-lic"/>
   <meta property="og:site_name" content="Neointeraction"/>
   <meta property="og:type" content="article"/>
   <meta name="google-site-verification" content="jDr4aVGcjUUHdNv8SD5bpXt6-kwDs5rDAbNqhXDrENY"/>
-  <meta property="article:author" content="Rakshanda Naik"/>
+  <meta property="article:author" content="Thirunavukkarasu Lakshumanan"/>
   <meta property="article:published_time" content="2024-01-25T10:58:02.450Z"/>
-  <meta property="article:modified_time" content="2024-03-21T04:54:56.696Z"/>
+  <meta property="article:modified_time" content="2024-08-23T04:54:56.696Z"/>
   <link rel="alternate" href="https://www.neointeraction.com/blog-feed.xml" title="Neointeraction - RSS" type="application/rss+xml"/>
   <meta name="twitter:card" content="summary_large_image"/>
-  <meta name="twitter:title" content="UX Design Guidelines for Mobile App Design"/>
-  <meta name="twitter:description" content="Discover the essential UX design guidelines that every mobile app designer must follow to create intuitive, user-friendly, and successful mobile applications."/>
-  <meta name="twitter:image" content="https://www.neointeraction.com/assets/images/blog/ux-design-guidelines-mobile-app/mobie-app-design_guidelines.jpeg"/>
+  <meta name="twitter:title" content="Effective UX Audit and Website Redesign to boost UX for LIC"/>
+  <meta name="twitter:description" content="Life Insurance of Corporation is a government-owned insurance firm in India. It is the largest insurance firm in the region, having a vast number of policyholders and agents. With many businesses going online, websites have become a big deal for all large corporations. And unfortunately, the current LIC website design appears to be out of date. The primary goal of the website is to provide a one-stop online solution to policyholders and insurance advisors, But the site's biggest flaw is the lack"/>
+  <meta name="twitter:image" content="https://static.wixstatic.com/media/c380a9_ba33f154326d4c51a38b3c71d85f6529~mv2.webp/v1/fill/w_941,h_727,al_c,q_85/c380a9_ba33f154326d4c51a38b3c71d85f6529~mv2.webp"/>
    
 </head>
 
@@ -42,82 +42,64 @@
     <section class="banner section-padding">
         <div class="container">
             <div class="blog-body-padding">
-    <p class="blog-author-text">
-        <div class="avatar-img-block">
-            <img src="<?php echo $path; ?>assets/images/blog/author/rakshanda-naik.avif" alt="avatar" class="avatar-img" />
-        </div>
-        <span>Rakshanda Naik • 25 Jan, 2024</span>
-    </p>
-    <h1 class="blog-title">UX design guidelines that must be followed while designing a mobile app</h1>
-    <div class="blog-banner-img">
-        <img src="<?php echo $path; ?>assets/images/blog/ux-design-guidelines-mobile-app/mobie-app-design_guidelines.jpeg" alt="mobie app design_guidelines" />
-    </div>
-    <p class="blog-body-text">
-        As of 2022, the world has over <a href="https://www.bankmycell.com/blog/how-many-phones-are-in-the-world" target="_blank">6.648 Billion</a> smartphone users – and that number is set to climb by a few hundred thousand in the next year alone as the mobile app industry is projected to reach 7.33 billion by 2025, Forecast figures by Ericsson & The Radicati Group. What is evident is that the industry is growing and will continue to expand aggressively in the coming decade. Every company understands the value of a user-centric <a href="https://www.neointeraction.com/cruiseship-crew-mobility-tracking-design" target="_blank">mobile app design</a>, which is the foundation for the app's usability and intuitiveness. This article addresses the foundations of mobile app UX design that can attract a large number of users.
-    </p>
-    <p class="blog-body-text">
-        A brilliant idea isn't enough to develop a successful app; mobile app design is also important. It is not difficult to create an app, but planning it with <a href="https://www.neointeraction.com/ui-ux-design-developer-services-company-bangalore" target="_blank">user experience design</a> requires time and work.
-    </p>
-    <div class="blog-quote-block">
-        <div class="quote-icon"></div>
-        <p class="blog-quote-text">Mobile apps should appeal to the user's intuition to guide them through the app.</p>
-    </div>
-    <h4 class="blog-body-title">Fundamentals of mobile UX design</h4>
-    <h4 class="blog-body-title">Make Interface Elements Clearly Visible</h4>
-    <div class="blog-banner-img">
-        <img src="<?php echo $path; ?>assets/images/blog/ux-design-guidelines-mobile-app/Interface-Elements_clear-design.jpeg" alt="Interface Elements_clear design" />
-    </div>
-    <p class="blog-body-text">
-        Users frequently use mobile apps in dimly lit areas or in the glare of the sun. The key to good mobile apps is intuitive design, which allows users to execute activities in the smallest amount of time possible. Designers must always consider the viewpoint of first-time users, as well as how fast and easily they can grasp the app's functionality and flow. Clarity teaches designers about which UI components are essential and which are nice-to-haves that cause clutter. The goal is to keep things simple, and focusing on design clarity will help you do that.
-    </p>
-    <h4 class="blog-body-title">Use Established Gestures</h4>
-    <div class="blog-banner-img">
-        <img src="<?php echo $path; ?>assets/images/blog/ux-design-guidelines-mobile-app/gestures_ux-design.jpeg" alt="gestures_ux design" />
-    </div>
-    <p class="blog-body-text">
-        When a user learns a particular gesture, it becomes an integral part of his or her overall user experience. There's a slim risk that this movement will be forgotten. One of the key benefits of gesture controls is that they reduce the number of steps and clicks required to complete a task in a mobile app. It takes a long time to delete an item by tapping it, browsing through the menu to find a relevant choice, and then clicking on it. It would take substantially less time to swipe that thing. The conversion rate will be higher if there are fewer clicks.
-    </p>
-    <h4 class="blog-body-title">Design Clear and Consistent Mobile App Navigation</h4>
-    <div class="blog-banner-img">
-        <img src="<?php echo $path; ?>assets/images/blog/ux-design-guidelines-mobile-app/consistent-navigation.jpeg" alt="consistent navigation" />
-    </div>
-    <p class="blog-body-text">
-        The ease with which consumers can learn how to utilize a mobile app is crucial. Users should be able to explore your app naturally using the clear pathways you've created. Using conventional navigation patterns seen in major operating systems, such as the Android navigation drawer and the iOS tab bar, is the best method to do this. The majority of customers are already familiar with these navigation design patterns, and incorporating them into your design will allow users to move through your app fast. For consistency, stick to one navigation scheme throughout your program.
-    </p>
-    <h4 class="blog-body-title">Create Seamless Animations and Transitions</h4>
-    <div class="blog-banner-img">
-        <img src="<?php echo $path; ?>assets/images/blog/ux-design-guidelines-mobile-app/mobile_animation_design.gif" alt="mobile_animation_design" />
-    </div>
-    <p class="blog-body-text">
-        To signify a state of transition, great mobile app designers use effective animation techniques across their products. It not only helps users intuitively comprehend the navigation mechanics of your app by adding life and character to the UI design, but it also allows them to recognize changes, swipe-throughs, and triggers of items. The usage of animation is an important aspect of the user experience. When it comes to transitions in mobile apps, there are numerous ways to communicate by utilizing animation. Sending the message, opening Settings, checking the box, and navigating to another page are examples of change. An animated transition is a great technique to reinforce the user's action.
-    </p>
-    <h4 class="blog-body-title">Typography</h4>
-    <div class="blog-banner-img">
-        <img src="<?php echo $path; ?>assets/images/blog/ux-design-guidelines-mobile-app/typography_ui-design.jpeg" alt="typography_ui design" />
-    </div>
-    <p class="blog-body-text">
-        Typography is the art of organizing written text, point sizes, line spacing, and other features in a way that the user experience is pleasant and readable. It is a skill that allows designers to translate human interactions into long-lasting visual forms. Fonts in mobile apps must be instantly legible. Because mobile devices have significantly less capacity than PCs or tablets, you should avoid overloading users' attention with information. Text on mobile devices must be at least 11 points tall to be legible at a typical viewing distance without the need to zoom in.
-    </p>
-    <h4 class="blog-body-title">Salient Points</h4>
-    <div class="blog-banner-img">
-        <img src="<?php echo $path; ?>assets/images/blog/ux-design-guidelines-mobile-app/user-interface-design_mobile-app.jpeg" alt="user interface design_mobile app" />
-    </div>
-    <ul>
-        <li>Uncluttered and clear interface elements help the user have a frustration-free user experience.</li>
-        <li>Using already established gestures helps the user perform tasks faster than clicking multiple times to get the same result.</li>
-        <li>Consistent navigation through the mobile app reduces confusion for the users</li>
-        <li>Seamless and smooth animations and navigation can help the user understand that actions are taken in response to their inputs.</li>
-        <li>Use of correct font, size, color, etc can guide users' attention to perform the needed task without overwhelming them.</li>
-    </ul>
-    <p class="blog-body-text">
-        Do you require assistance in developing the ideal app? Recruit <a href="https://www.neointeraction.com/ui-ux-design-developer-services-company-bangalore" target="_blank">UI designers & UX developers</a> from Neointeraction team to design the ultimate mobile application that your customers would adore.
-    </p>
-    <a href="<?php echo $path; ?>blogs.php" class="view-all-blogs-btn" style="text-decoration: none;">
-        <button class="btn btn-custom btn-custom-secondary">
-            View All Blogs
-        </button>
-    </a>
-</div>
+                <p class="blog-author-text">
+                    <div class="avatar-img-block">
+                        <img src="<?php echo $path; ?>assets/images/blog/author/.png" alt="avatar" class="avatar-img" />
+                    </div>
+                    <span> • 25 Jan, 2024</span>
+                </p>
+                <h1 class="banner-title">
+                Effective UX Audit and Website Redesign to boost UX for LIC
+                </h1>
+                <div class="blog-banner-img">
+                    <img src="<?php echo $path; ?>assets/images/blog/effective-ux-audit/image1.jpg" alt="blog1" />
+                </div>
+                <p class="blog-body-text">
+                    Life Insurance of Corporation is a government-owned insurance firm in India. It is the largest insurance firm in the region, having a vast number of policyholders and agents. With many businesses going online, websites have become a big deal for all large corporations. And unfortunately, the current LIC website design appears to be out of date. The primary goal of the website is to provide a one-stop online solution to policyholders and insurance advisors, But the site's biggest flaw is the lack of a good user experience. As one of the top ui ux design companies we truly enjoyed redesigning the ux audit for LIC.
+                </p>
+                <h4 class="blog-body-title">
+                    Identified Problems
+                </h4>
+
+                <p class="blog-body-text">
+                    <b>Problems related to the UX:</b>
+                    <ul>
+                        <li>The placement of the banner was unclear.</li>
+                        <li>There are too many undesired options on the navigation bar.</li>
+                        <li>Although paying premiums is the most common option for policyholders on the LIC website, it was not prominently shown on the landing page.</li>
+                        <li>Some key items, such as Buy Insurance, Renew Policy, Claim Policy, Claim status, and Premium Calculator, are not highlighted and are difficult to locate.</li>
+                        <li>Users' download links for mobile apps were misplaced.</li>
+                        <li>The landing page is clogged with unnecessary information and content.</li>
+                        <li>The footer options are cluttered and unorganized.</li>
+                    </ul>
+                </p>
+
+                <div class="blog-quote-block">
+                    <div class="quote-icon"></div>
+                    <p class="blog-quote-text">The only constant in the world is “CHANGE". Though this line has become a cliche, it is very important to implement the same in our operation. And of course, the change needs to be planned well considering all the right and critical factors.</p>
+                </div>
+
+               
+                <p class="blog-body-text">
+                    <a href="" target="_blank">here</a>.
+                </p>
+
+               
+
+                <h4 class="blog-body-title">Conclusion</h4>
+
+                <p class="blog-body-text">
+                    UX Audit is a necessary aspect of the design process in order to create a solution that meets both business and user requirements. If we focus on the details of the product and services, we tend to overlook the users and their perspectives. UX strategy and research play an important role in the process of product design. The most important factor of this project was that users might easily be lost due to a bad online presence experience.
+                </p>
+
+
+                <a href="<?php echo $path; ?>blogs.php" class="view-all-blogs-btn" style="text-decoration: none;">
+                    <button class="btn btn-custom btn-custom-secondary">
+                        View All Blogs
+                    </button>
+                </a>
+
+            </div>
             <?php include $path.'components/submit-enquiry.php'; ?>
         </div>
 
