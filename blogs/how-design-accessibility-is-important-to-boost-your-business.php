2242685f25fb95cda4d0343e5d9fde96ccee043e
vmidhun <vmidhun@gmail.com>
2025-10-14 15:16:17 +0530

    Blog Updated how-design-accessibility-is-important-to-boost-your-business

--- blogs/how-design-accessibility-is-important-to-boost-your-business.php
+++ blogs/how-design-accessibility-is-important-to-boost-your-business.php
@@ -6,26 +6,26 @@
   <?php include $path.'includes/favicon.php'; ?>
   <?php include $path.'includes/css.php'; ?>
   <?php include $path.'includes/head-additional-scripts.php'; ?>
-  <title>Effective UX Audit and Website Redesign to boost UX for LIC</title>
-  <meta name="description" content="Design case study on Effective UX Audit and Redesigning LIC Website to boos user experience"/>
-  <link rel="canonical" href="https://www.neointeraction.com/post/effective-ux-audit-and-website-redesign-to-boost-ux-for-lic"/>
-  <meta property="og:title" content="Effective UX Audit and Website Redesign to boost UX for LIC"/>
-  <meta property="og:description" content="Life Insurance of Corporation is a government-owned insurance firm in India. It is the largest insurance firm in the region, having a vast number of policyholders and agents. With many businesses going online, websites have become a big deal for all large corporations. And unfortunately, the current LIC website design appears to be out of date. The primary goal of the website is to provide a one-stop online solution to policyholders and insurance advisors, But the site's biggest flaw is the lack"/>
-  <meta property="og:image" content="https://static.wixstatic.com/media/c380a9_ba33f154326d4c51a38b3c71d85f6529~mv2.webp/v1/fill/w_941,h_727,al_c,q_85/c380a9_ba33f154326d4c51a38b3c71d85f6529~mv2.webp"/>
+  <title>How Design Accessibility is Important to Boost Your Business</title>
+  <meta name="description" content="Discover how accessible design can broaden your audience, enhance user experience, and boost your business. Learn key strategies and see real-world examples."/>
+  <link rel="canonical" href="https://www.neointeraction.com/blogs/how-design-accessibility-is-important-to-boost-your-business.php"/>
+  <meta property="og:title" content="How Design Accessibility is Important to Boost Your Business"/>
+  <meta property="og:description" content="Discover how accessible design can broaden your audience, enhance user experience, and boost your business. Learn key strategies and see real-world examples."/>
+  <meta property="og:image" content="https://www.neointeraction.com/assets/images/blog/design-accessibility-business-boost/accessibility-design-for-business.jpeg"/>
   <meta property="og:image:width" content="941"/>
   <meta property="og:image:height" content="727"/>
-  <meta property="og:url" content="https://www.neointeraction.com/post/effective-ux-audit-and-website-redesign-to-boost-ux-for-lic"/>
+  <meta property="og:url" content="https://www.neointeraction.com/blogs/how-design-accessibility-is-important-to-boost-your-business.php"/>
   <meta property="og:site_name" content="Neointeraction"/>
   <meta property="og:type" content="article"/>
   <meta name="google-site-verification" content="jDr4aVGcjUUHdNv8SD5bpXt6-kwDs5rDAbNqhXDrENY"/>
-  <meta property="article:author" content="Thirunavukkarasu Lakshumanan"/>
-  <meta property="article:published_time" content="2024-01-25T10:58:02.450Z"/>
+  <meta property="article:author" content="Sahla Feroc"/>
+  <meta property="article:published_time" content="2023-12-22T10:58:02.450Z"/>
   <meta property="article:modified_time" content="2024-08-23T04:54:56.696Z"/>
   <link rel="alternate" href="https://www.neointeraction.com/blog-feed.xml" title="Neointeraction - RSS" type="application/rss+xml"/>
   <meta name="twitter:card" content="summary_large_image"/>
-  <meta name="twitter:title" content="Effective UX Audit and Website Redesign to boost UX for LIC"/>
-  <meta name="twitter:description" content="Life Insurance of Corporation is a government-owned insurance firm in India. It is the largest insurance firm in the region, having a vast number of policyholders and agents. With many businesses going online, websites have become a big deal for all large corporations. And unfortunately, the current LIC website design appears to be out of date. The primary goal of the website is to provide a one-stop online solution to policyholders and insurance advisors, But the site's biggest flaw is the lack"/>
-  <meta name="twitter:image" content="https://static.wixstatic.com/media/c380a9_ba33f154326d4c51a38b3c71d85f6529~mv2.webp/v1/fill/w_941,h_727,al_c,q_85/c380a9_ba33f154326d4c51a38b3c71d85f6529~mv2.webp"/>
+  <meta name="twitter:title" content="How Design Accessibility is Important to Boost Your Business"/>
+  <meta name="twitter:description" content="Discover how accessible design can broaden your audience, enhance user experience, and boost your business. Learn key strategies and see real-world examples."/>
+  <meta name="twitter:image" content="https://www.neointeraction.com/assets/images/blog/design-accessibility-business-boost/accessibility-design-for-business.jpeg"/>
    
 </head>
 
@@ -42,56 +42,50 @@
     <section class="banner section-padding">
         <div class="container">
             <div class="blog-body-padding">
-                <p class="blog-author-text">
+     <p class="blog-author-text">
                     <div class="avatar-img-block">
-                        <img src="<?php echo $path; ?>assets/images/blog/author/.png" alt="avatar" class="avatar-img" />
+                        <img src="<?php echo $path; ?>assets/images/blog/author/sahla-feroc.avif" alt="avatar" class="avatar-img" />
                     </div>
-                    <span> • 25 Jan, 2024</span>
+                    <span>Sahla Feroc • 22 Dec, 2023</span>
                 </p>
-                <h1 class="banner-title">
-                Effective UX Audit and Website Redesign to boost UX for LIC
-                </h1>
-                <div class="blog-banner-img">
-                    <img src="<?php echo $path; ?>assets/images/blog/effective-ux-audit/image1.jpg" alt="blog1" />
-                </div>
-                <p class="blog-body-text">
-                    Life Insurance of Corporation is a government-owned insurance firm in India. It is the largest insurance firm in the region, having a vast number of policyholders and agents. With many businesses going online, websites have become a big deal for all large corporations. And unfortunately, the current LIC website design appears to be out of date. The primary goal of the website is to provide a one-stop online solution to policyholders and insurance advisors, But the site's biggest flaw is the lack of a good user experience. As one of the top ui ux design companies we truly enjoyed redesigning the ux audit for LIC.
-                </p>
-                <h4 class="blog-body-title">
-                    Identified Problems
-                </h4>
-
-                <p class="blog-body-text">
-                    <b>Problems related to the UX:</b>
-                    <ul>
-                        <li>The placement of the banner was unclear.</li>
-                        <li>There are too many undesired options on the navigation bar.</li>
-                        <li>Although paying premiums is the most common option for policyholders on the LIC website, it was not prominently shown on the landing page.</li>
-                        <li>Some key items, such as Buy Insurance, Renew Policy, Claim Policy, Claim status, and Premium Calculator, are not highlighted and are difficult to locate.</li>
-                        <li>Users' download links for mobile apps were misplaced.</li>
-                        <li>The landing page is clogged with unnecessary information and content.</li>
-                        <li>The footer options are cluttered and unorganized.</li>
-                    </ul>
-                </p>
-
-                <div class="blog-quote-block">
-                    <div class="quote-icon"></div>
-                    <p class="blog-quote-text">The only constant in the world is “CHANGE". Though this line has become a cliche, it is very important to implement the same in our operation. And of course, the change needs to be planned well considering all the right and critical factors.</p>
-                </div>
-
-               
-                <p class="blog-body-text">
-                    <a href="" target="_blank">here</a>.
-                </p>
-
-               
-
-                <h4 class="blog-body-title">Conclusion</h4>
-
-                <p class="blog-body-text">
-                    UX Audit is a necessary aspect of the design process in order to create a solution that meets both business and user requirements. If we focus on the details of the product and services, we tend to overlook the users and their perspectives. UX strategy and research play an important role in the process of product design. The most important factor of this project was that users might easily be lost due to a bad online presence experience.
-                </p>
-
+    <h1 class="blog-title">How design accessibility is important to boost your business</h1>
+    
+    <div class="blog-banner-img">
+        <img src="<?php echo $path; ?>assets/images/blog/design-accessibility-business-boost/accessibility-design-for-business.jpeg" alt="accessibility design for business" />
+    </div>
+    <p class="blog-body-text">In today's diverse world, businesses know it's important to make their products and services accessible to everyone. Accessible design is the key to crafting digital experiences that cater to all, regardless of ability. Now, let's look at the numerous benefits that accessible design can provide to businesses.</p>
+    <h4 class="blog-body-title">Reaching a Broader Audience</h4>
+    <p class="blog-body-text">Accessible design stands as a pivotal asset, wielding the power to extend a brand's reach across a diverse demographic spectrum. The World Health Organization reports that approximately 1.3 billion people, accounting for 16% of the global population, live with different abilities. By using accessible design, businesses not only reach this market but also show that they care about creating digital experiences for everyone.</p>
+    <h4 class="blog-body-title">Better User Experience for All</h4>
+    <p class="blog-body-text">Accessible design, aligned with user-centered design principles, aims to make everyone's experience better. It not only boosts user engagement but also makes users happier and more likely to stick around. Features such as clear navigation, readable fonts, and adaptable layouts, designed with accessibility in mind, resonate not only with individuals with different abilities but also with the general user population.</p>
+    <h4 class="blog-body-title">Positive Brand Reputation</h4>
+    <p class="blog-body-text">In a world that values inclusivity, businesses embracing accessible design can craft a positive brand image, signaling their care for everyone and fostering trust and loyalty. This commitment goes beyond just reputation-building; it positions companies as ethical leaders. It not only deepens trust and loyalty among existing customers but also attracts new ones actively seeking brands that prioritize accessibility.</p>
+    <h4 class="blog-body-title">Boosting Search Engine Ranking</h4>
+    <p class="blog-body-text">The success of a website is determined not only by its aesthetic appeal but also by its discoverability via search engines. A frequently neglected factor that can substantially impact a website's search engine ranking (SEO) is its accessible design. Search engines, like Google, value accessibility as it aligns with their goal of providing the best possible user experience. So, businesses investing in accessible design not only reach more people but also climb the online visibility ladder.</p>
+    <h4 class="blog-body-title">Legal Safety Net</h4>
+    <p class="blog-body-text">Accessible design not only improves user experience but also has legal implications for businesses. Prioritizing accessibility is a proactive move that reduces the likelihood of legal complications while also complying with global inclusivity laws. Improving website accessibility through adherence to the Web Content Accessibility Guidelines (WCAG) not only ensures a more user-friendly experience but also mitigates legal concerns. Businesses that prioritize accessible design can reach a wider audience, comply with regulations, and establish a more inclusive and legally secure digital space.</p>
+    <div class="blog-quote-block">
+        <div class="quote-icon"></div>
+        <p class="blog-quote-text">Accessible design is not just a technical requirement; it's a moral imperative. It's about giving everyone the opportunity to participate fully in society. - Jeffrey Zeldman</p>
+    </div>
+    <h4 class="blog-body-title">How to Make Your Designs More Accessible</h4>
+    <ol>
+        <li><strong>Alt Text for Images:</strong> Ensure every image on your website has descriptive alt text. This not only aids users with visual impairments but also improves your site's SEO.</li>
+        <li><strong>Readable Fonts and Contrasts:</strong> Use fonts that are clear and easy to read, and make sure the text and background colors contrast sharply. This helps people with visual impairments and makes it easier for everyone to read.</li>
+        <li><strong>Logical Heading Structure:</strong> Organize content with a logical heading structure. This helps screen readers and users to navigate your content easily, improving the overall user experience.</li>
+        <li><strong>Keyboard Accessibility:</strong> Design your website so that all functionalities are accessible using a keyboard. This is crucial for users who rely on keyboard navigation or have motor disabilities.</li>
+        <li><strong>Transcripts for Multimedia:</strong> Create transcripts for audio and video content to make it accessible to deaf or hard-of-hearing people and reach a wider audience.</li>
+        <li><strong>Responsive Design:</strong> Design your website to be responsive so that it can adapt to different screen sizes and devices. This will ensure that your website is accessible and user-friendly for everyone.</li>
+        <li><strong>Regular Accessibility Audits:</strong> To keep your website accessible as it evolves, make it a habit to conduct regular accessibility audits as part of your routine maintenance. This proactive practice helps you catch and address potential issues at an early stage.</li>
+        <li><strong>User Testing with Diverse Audiences:</strong> Enhance the accessibility of your product by testing it with users of varying abilities. Their insights can help you identify areas where your product can be more inclusive.</li>
+    </ol>
+    <h4 class="blog-body-title">Case Study: Netflix's Accessibility Success</h4>
+    <p class="blog-body-text">In 2019, Netflix made its platform more accessible to people with different abilities by adding features such as audio descriptions, subtitles, and keyboard navigation. The impact? A 50% increase in subscribers with disabilities, highlighting the tangible benefits of accessible design. This case showcases how prioritizing accessibility goes beyond compliance – a strategic move that enhances brand reputation and taps into a substantial market segment.</p>
+    <div class="blog-banner-img">
+        <img src="<?php echo $path; ?>assets/images/blog/design-accessibility-business-boost/netflix-accessibility-design.jpeg" alt="Netflix accessibility design" />
+    </div>
+    <h4 class="blog-body-title">Conclusion</h4>
+    <p class="blog-body-text">Accessible design is more than just following rules; it's about making digital spaces welcoming for everyone. Businesses that prioritize accessible design are not only keeping up with evolving technology standards, but also building a digital world that is equitable, inclusive, and prepared for the future.</p>
 
                 <a href="<?php echo $path; ?>blogs.php" class="view-all-blogs-btn" style="text-decoration: none;">
                     <button class="btn btn-custom btn-custom-secondary">
