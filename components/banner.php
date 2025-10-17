33f93b4e2c8daafdf8ebaec8e7a625a17ef9cc54
vmidhun <vmidhun@gmail.com>
2025-10-17 16:47:03 +0530

    home page banner fixed

---
+++ components/banner.php
@@ -0,0 +1,23 @@
+    <section class="banner section-padding">
+        <div class="banner-slider pb-5">
+            <div>
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="banner-content text-center">
+                                <h1 class="banner-title">
+                                    Award winning <span class="highlight">Ux Agency</span>
+                                </h1>
+                                 <h4 class="banner-supertext">research-to-design process that differentiates professional UX work</h4>
+                                <button class="btn btn-custom btn-custom-secondary banner-btn consultation-btn" data-bs-toggle="modal" data-bs-target="#registerModal">
+                                    Book A Consultation
+                                </button>
+                            </div>
+                        </div>
+                        
+                    </div>
+                </div>
+            </div>
+          
+        </div>
+    </section>
--- index.php
+++ index.php
@@ -33,33 +33,7 @@
         <?php include $path . 'components/navigation.php'; ?>
         <?php include $path . 'components/whatsapp.php'; ?>
     </div>
-    <section class="banner section-padding">
-        <div class="banner-slider pb-5">
-            <div>
-                <div class="container">
-                    <div class="row">
-                        <div class="col-md-8">
-                            <div class="banner-content">
-                                <h4 class="banner-supertext">research-to-design process that differentiates professional UX work</h4>
-                                <h1 class="banner-title">
-                                    Award winning <span class="highlight">Ux Agency</span>
-                                </h1>
-                                <button class="btn btn-custom btn-custom-secondary banner-btn consultation-btn" data-bs-toggle="modal" data-bs-target="#registerModal">
-                                    Book A Consultation
-                                </button>
-                            </div>
-                        </div>
-                        <div class="col-md-4">
-                            <div class="banner-img-block">
-                                <img src="<?php echo $path; ?>assets/images/banner/globe.jpg" alt="banner" >
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-          
-        </div>
-    </section>
+ <?php include $path . 'components/banner.php'; ?>
     <section class="business-success section-padding">
         <div class="container">
             <h2 class="section-title">
@@ -280,7 +254,7 @@
         $imageAlt = "Contact us";
         include $path . 'components/hire-designers-engineers.php'; 
     ?>
-    <?php include $path . 'components/join-our-team.php'; ?>
+
     <?php include $path . 'components/footer.php'; ?>
     <?php include $path . 'includes/footer-additional-scripts.php'; ?>
     <?php include $path . 'includes/js.php'; ?>
