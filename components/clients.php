ffc36d179d69f1631d11942b2aef5a9bb2616264
vmidhun <vmidhun@gmail.com>
2025-10-21 18:41:09 +0530

    clients images updated

--- components/clients.php
+++ components/clients.php
@@ -12,12 +12,12 @@ $filtered_clients = array_filter($clients_data, function($client) use ($tag) {
 <div class="client-logo-flex" aria-hidden="true">
     <?php foreach ($filtered_clients as $client) { ?>
         <div class="clf-item">
-            <img src="<?php echo $path . $client['image']; ?>" alt="<?php echo $client['name']; ?>" >
+            <img src="<?php echo $path . $client['image']; ?>" alt="<?php echo $client['name']; ?>" width="<?php echo $client['width']; ?>" height="<?php echo $client['height']; ?>">
         </div>
     <?php } ?>
     <?php foreach ($filtered_clients as $client) { ?>
         <div class="clf-item">
-            <img src="<?php echo $path . $client['image']; ?>" alt="<?php echo $client['name']; ?>" >
+            <img src="<?php echo $path . $client['image']; ?>" alt="<?php echo $client['name']; ?>" width="<?php echo $client['width']; ?>" height="<?php echo $client['height']; ?>">
         </div>
     <?php } ?>
 </div>
