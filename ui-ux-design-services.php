61bd2174d60922cc822ea1240b371adbcc47be80
vmidhun <vmidhun@gmail.com>
2025-10-28 13:11:05 +0530

    industry experience component updated

--- ui-ux-design-services.php
+++ ui-ux-design-services.php
@@ -339,73 +339,7 @@
     </section>
     <!-- dev-cycle  end -->
     <!-- industry-experience  -->
-    <section class="industry-experience section-padding">
-        <div class="container">
-            <h2 class="section-title">Industry Experience</h2>
-            <p class="body-text">Neointeraction Design is an expert frontend development company in India that specialize in implementing pixel-perfect UI designs across diverse industries, driving successful outcomes for enterprise solutions, startup ventures, and B2C products enhancing user experiences and boosts business performance.</p>
-            <div class="what-we-do-block section-margin">
-                <div class="row">
-                    <div class="col-6 col-md-3 col-lg-3">
-                        <div class="wwd-item">
-                            <img src="<?php echo $path; ?>assets/images/industry-experience/ob.svg" alt="Online banking solutions" />
-                            <h3 class="wwd-title">Online Banking</h3>
-                            <p class="wwd-text">Revolutionizing financial interactions with intuitive interfaces and secure transactions.</p>
-                        </div>
-                    </div>
-                    <div class="col-6 col-md-3 col-lg-3">
-                        <div class="wwd-item">
-                            <img src="<?php echo $path; ?>assets/images/industry-experience/cm.svg" alt="Capital market platforms" />
-                            <h3 class="wwd-title">Capital Market</h3>
-                            <p class="wwd-text">Empowering investors with data-driven insights and intuitive trading platforms.</p>
-                        </div>
-
-                    </div>
-                    <div class="col-6 col-md-3 col-lg-3">
-                        <div class="wwd-item">
-                            <img src="<?php echo $path; ?>assets/images/industry-experience/healthcare.svg" alt="Healthcare digital solutions" />
-                            <h3 class="wwd-title">Healthcare</h3>
-                            <p class="wwd-text">Redefining patient care through user-centric healthcare platforms, enhancing wellness journeys. </p>
-                        </div>
-                    </div>
-                    <div class="col-6 col-md-3 col-lg-3">
-                        <div class="wwd-item">
-                            <img src="<?php echo $path; ?>assets/images/industry-experience/iot.svg" alt="IoT ecosystem solutions" />
-                            <h3 class="wwd-title">IoT</h3>
-                            <p class="wwd-text">Connecting the world through seamless digital ecosystems, powering smarter user experiences.</p>
-                        </div>
-                    </div>
-                    <div class="col-6 col-md-3 col-lg-3">
-                        <div class="wwd-item">
-                            <img src="<?php echo $path; ?>assets/images/industry-experience/th.svg" alt="Travel and hospitality platforms" />
-                            <h3 class="wwd-title">Travel and Hospitality</h3>
-                            <p class="wwd-text">Crafting memorable customer digital journeys, from booking to exploration, for unforgettable experiences.</p>
-                        </div>
-                    </div>
-                    <div class="col-6 col-md-3 col-lg-3">
-                        <div class="wwd-item">
-                            <img src="<?php echo $path; ?>assets/images/industry-experience/es.svg" alt="Enterprise digital solutions" />
-                            <h3 class="wwd-title">Enterprise Solutions</h3>
-                            <p class="wwd-text">Tailored digital solutions that streamline operations and drive scalable growth.</p>
-                        </div>
-                    </div>
-                    <div class="col-6 col-md-3 col-lg-3">
-                        <div class="wwd-item">
-                            <img src="<?php echo $path; ?>assets/images/industry-experience/ob.svg" alt="Startup digital solutions" />
-                            <h3 class="wwd-title">Startup Ventures</h3>
-                            <p class="wwd-text">Driving digital innovation with agile solutions that disrupt markets and fuel rapid growth.</p>
-                        </div>
-                    </div>
-                    <div class="col-6 col-md-3 col-lg-3">
-                        <div class="wwd-item">
-                            <img src="<?php echo $path; ?>assets/images/industry-experience/es.svg" alt="B2C digital platforms" />
-                            <h3 class="wwd-title">B2C</h3>
-                            <p class="wwd-text">Improving consumer experiences with engaging and intuitive digital platforms.</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section>
+    <?php include $path . 'components/industry-experience.php'; ?>
     <!-- industry-experience  end -->
     <?php include $path . 'components/registration-modal.php'; ?>
     <?php $faq_tag = 'services';
--- ux-design-services.php
+++ ux-design-services.php
@@ -336,76 +336,7 @@ include_once 'includes/tracking-functions.php';
         </div>
     </section>
 
-
-
-    <section class="industry-experience section-padding mb-5">
-        <div class="container">
-            <h2 class="section-title">Industry Experience</h2>
-            <p class="body-text">Neointeraction Design is an expert frontend development company in India that specialize in implementing pixel-perfect UI designs across diverse industries, driving successful outcomes for enterprise solutions, startup ventures, and B2C products enhancing user experiences and boosts business performance.</p>
-            <div class="what-we-do-block section-margin">
-                <div class="row">
-                    <div class="col-6 col-md-3 col-lg-3">
-                        <div class="wwd-item">
-                            <img src="<?php echo $path; ?>assets/images/industry-experience/ob.svg" alt="ob" />
-                            <h4 class="wwd-title">Online Banking</h4>
-                            <p class="wwd-text">Revolutionizing financial interactions with intuitive interfaces and secure transactions.</p>
-                        </div>
-                    </div>
-                    <div class="col-6 col-md-3 col-lg-3">
-                        <div class="wwd-item">
-                            <img src="<?php echo $path; ?>assets/images/industry-experience/cm.svg" alt="cm" />
-                            <h4 class="wwd-title">Capital Market</h4>
-                            <p class="wwd-text">Empowering investors with data-driven insights and intuitive trading platforms.</p>
-                        </div>
-
-                    </div>
-                    <div class="col-6 col-md-3 col-lg-3">
-                        <div class="wwd-item">
-                            <img src="<?php echo $path; ?>assets/images/industry-experience/healthcare.svg" alt="healthcare" />
-                            <h4 class="wwd-title">Healthcare</h4>
-                            <p class="wwd-text">Redefining patient care through user-centric healthcare platforms, enhancing wellness journeys. </p>
-                        </div>
-                    </div>
-                    <div class="col-6 col-md-3 col-lg-3">
-                        <div class="wwd-item">
-                            <img src="<?php echo $path; ?>assets/images/industry-experience/iot.svg" alt="iot" />
-                            <h4 class="wwd-title">IoT</h4>
-                            <p class="wwd-text">Connecting the world through seamless digital ecosystems, powering smarter user experiences.</p>
-                        </div>
-                    </div>
-                    <div class="col-6 col-md-3 col-lg-3">
-                        <div class="wwd-item">
-                            <img src="<?php echo $path; ?>assets/images/industry-experience/th.svg" alt="th" />
-                            <h4 class="wwd-title">Travel and Hospitality</h4>
-                            <p class="wwd-text">Crafting memorable customer digital journeys, from booking to exploration, for unforgettable experiences.</p>
-                        </div>
-                    </div>
-                    <div class="col-6 col-md-3 col-lg-3">
-                        <div class="wwd-item">
-                            <img src="<?php echo $path; ?>assets/images/industry-experience/es.svg" alt="es" />
-                            <h4 class="wwd-title">Enterprise Solutions</h4>
-                            <p class="wwd-text">Tailored digital solutions that streamline operations and drive scalable growth.</p>
-                        </div>
-                    </div>
-                    <div class="col-6 col-md-3 col-lg-3">
-                        <div class="wwd-item">
-                            <img src="<?php echo $path; ?>assets/images/industry-experience/ob.svg" alt="ob" />
-                            <h4 class="wwd-title">Startup Ventures</h4>
-                            <p class="wwd-text">Driving digital innovation with agile solutions that disrupt markets and fuel rapid growth.</p>
-                        </div>
-                    </div>
-                    <div class="col-6 col-md-3 col-lg-3">
-                        <div class="wwd-item">
-                            <img src="<?php echo $path; ?>assets/images/industry-experience/es.svg" alt="es" />
-                            <h4 class="wwd-title">B2C</h4>
-                            <p class="wwd-text">Enhancing consumer experiences with engaging and intuitive digital platforms</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section>
-
+    <?php include $path . 'components/industry-experience.php'; ?>
     <?php 
         $title = 'Start Your Consultation'; 
         $desc = 'Transform your ideas into pixel-perfect digital experiences with our expert designers and front-end engineers.';
